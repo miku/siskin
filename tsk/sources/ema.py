8b201eeaf24be273a6752004020a9937a67ffa9f
Martin Czygan <martin.czygan@gmail.com>
2014-06-23 23:45:56 +0200

    added ema/009

---
+++ tsk/sources/ema.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,E1103,C0301
+
+"""
+EMA.
+
+Configuration keys:
+
+[ema]
+
+scp-src = [user@server:[port]]/path/to/file.mrc
+"""
+
+from gluish.benchmark import timed
+from gluish.esindex import CopyToIndex
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+from tsk.task import DefaultTask
+import datetime
+import luigi
+
+# get config
+from tsk.configuration import TskConfig
+config = TskConfig.instance()
+
+class EMATask(DefaultTask):
+    TAG = '009'
+
+    def closest(self):
+        return datetime.date(2014, 1, 1)
+
+class EMASync(EMATask):
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    @timed
+    def run(self):
+        output = shellout("scp {origin} {output}", origin=config.get('ema', 'scp-src'))
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='mrc'))
+
+class EMAJson(EMATask):
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return EMASync(date=self.date)
+
+    @timed
+    def run(self):
+        output = shellout("marctojson -m date={date} {input} > {output}",
+                          input=self.input().path, date=self.closest())
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='ldj'))
+
+class EMAIndex(EMATask, CopyToIndex):
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    index = 'ema'
+    doc_type = 'title'
+    purge_existing_index = True
+
+    mapping = {'title': {'date_detection': False,
+                          '_id': {'path': 'content.001'},
+                          '_all': {'enabled': True,
+                                   'term_vector': 'with_positions_offsets',
+                                   'store': True}}}
+    def update_id(self):
+        """ This id will be a unique identifier for this indexing task."""
+        return self.effective_task_id()
+
+    def requires(self):
+        return EMAJson(date=self.date)
