75af2288d72695072da74dd4d47c9b6eb2eaed32
Martin Czygan <martin.czygan@gmail.com>
2014-06-25 01:49:32 +0200

    added install_requires to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ try:
 except:
     from distutils.core import setup
 
-
 setup(name='siskin',
       version='0.0.3',
       description='Various sources and workflows.',
@@ -36,5 +35,33 @@ setup(name='siskin',
         'bin/taskredo',
         'bin/taskrm',
         'bin/taskwc',
-      ]
+      ],
+      install_requires=[
+        "BeautifulSoup==3.2.1",
+        "MySQL-python==1.2.5",
+        "argparse==1.2.1",
+        "astroid==1.1.1",
+        "colorama==0.3.1",
+        "distribute==0.6.34",
+        "elasticsearch==1.0.0",
+        "gluish==0.1.52",
+        "gspread==0.2.1",
+        "logilab-common==0.61.0",
+        "luigi==1.0.16",
+        "lxml==3.3.5",
+        "nose==1.3.3",
+        "numpy==1.8.1",
+        "openpyxl==1.8.6",
+        "pandas==0.14.0",
+        "pyisbn==1.0.0",
+        "pylint==1.2.1",
+        "pymarc==3.0.1",
+        "python-dateutil==2.2",
+        "pytz==2014.4",
+        "requests==2.3.0",
+        "six==1.7.2",
+        "sqlitebck==1.2.1",
+        "urllib3==1.8.2",
+        "wsgiref==0.1.2",
+      ],
 )
