60d8266351e04012da740864ce1320dd1bf5681b
Martin Czygan <martin.czygan@gmail.com>
2014-06-24 21:07:29 +0200

    create temporary directory if necessary

--- siskin/__init__.py
+++ siskin/__init__.py
@@ -1,7 +1,16 @@
 # coding: utf-8
 # pylint: disable=C0103
-import tempfile
+
+from __future__ import print_function
 from siskin.configuration import Config
+import os
+import tempfile
 
 config = Config.instance()
 tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
+
+if not os.path.exists(tempfile.tempdir):
+    try:
+        os.makedirs(tempfile.tempdir, 1777)
+    except OSError as err:
+        print('temp dir does not exists and we cannot create it: {}'.format(tempfile.tempdir))
