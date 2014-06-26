412eb6cdcb3e00158a5e1b0ec6452618199c8c67
Martin Czygan <martin.czygan@gmail.com>
2014-06-26 23:25:50 +0200

    added imdb

---
+++ siskin/sources/imdb.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,R0904,E1103
+
+"""
+IMDB.
+"""
+
+from gluish.common import FTPMirror
+from gluish.intervals import hourly
+from siskin.task import DefaultTask
+import luigi
+
+class IMDBTask(DefaultTask):
+    TAG = 'imdb'
+
+class IMDBSync(IMDBTask):
+
+    indicator = luigi.Parameter(default=hourly())
+
+    def requires(self):
+        return FTPMirror(host='ftp.fu-berlin.de',
+                         username='anonymous',
+                         password='',
+                         base='/pub/misc/movies/database/')
+
+    def run(self):
+        self.input().move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
