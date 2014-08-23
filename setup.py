79161f280928212f8d2782838f4ad9102529d186
Martin Czygan <martin.czygan@gmail.com>
2014-08-23 20:46:01 +0200

    v0.0.46

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
     from distutils.core import setup
 
 setup(name='siskin',
-      version='0.0.45',
+      version='0.0.46',
       description='Various sources and workflows.',
       url='https://github.com/miku/siskin',
       author='Martin Czygan',
--- siskin/__init__.py
+++ siskin/__init__.py
@@ -12,7 +12,7 @@ import tempfile
 import warnings
 warnings.filterwarnings("ignore")
 
-__version__ = '0.0.45'
+__version__ = '0.0.46'
 
 config = Config.instance()
 tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
