9a84267387396ec2ce83028a6a5ac748d8822ced
Martin Czygan <martin.czygan@gmail.com>
2014-08-25 17:12:57 +0200

    v0.0.48

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
     from distutils.core import setup
 
 setup(name='siskin',
-      version='0.0.47',
+      version='0.0.48',
       description='Various sources and workflows.',
       url='https://github.com/miku/siskin',
       author='Martin Czygan',
