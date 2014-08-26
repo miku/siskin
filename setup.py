cdae5df60d18dd46e4a6a29048eb1885b4ae7cfa
Martin Czygan <martin.czygan@gmail.com>
2014-08-26 16:38:15 +0200

    up gluish requirement

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
     from distutils.core import setup
 
 setup(name='siskin',
-      version='0.0.52',
+      version='0.0.53',
       description='Various sources and workflows.',
       url='https://github.com/miku/siskin',
       author='Martin Czygan',
@@ -57,7 +57,7 @@ setup(name='siskin',
         'colorama==0.2.7',
         'decorator==3.4.0',
         'elasticsearch==1.2.0',
-        'gluish==0.1.64',
+        'gluish==0.1.65',
         'gspread==0.2.1',
         'jsonpath-rw==1.3.0',
         'logilab-common==0.61.0',
