fb7fb8b0638f2df78e6fcaca309e8d1f9b813125
Martin Czygan <martin.czygan@gmail.com>
2014-09-23 03:45:18 +0200

    use luigi 1.0.19

--- siskin/__init__.py
+++ siskin/__init__.py
@@ -12,7 +12,7 @@ import tempfile
 import warnings
 warnings.filterwarnings("ignore")
 
-__version__ = '0.0.66'
+__version__ = '0.0.67'
 
 config = Config.instance()
 tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
