9b5c2d3b99736c7e990ea8f1cf43ec1006d9901c
Martin Czygan <martin.czygan@gmail.com>
2014-10-15 17:22:30 +0200

    added GNDVirtuoso
    * install virtuoso
    * clear any previous graph with
    
        SPARQL CLEAR GRAPH <http://d-nb.info/gnd/>;
    
    * allow `dirname $(taskoutput GNDNTriplesSplitted)` in
      /etc/virtuoso-opensource-6.1/virtuoso.ini
      under AllowedDirs

--- siskin/sources/gnd.py
+++ siskin/sources/gnd.py
@@ -13,6 +13,7 @@ from gluish.esindex import CopyToIndex
 from gluish.format import TSV
 from gluish.intervals import monthly
 from gluish.parameter import ClosestDateParameter
+from gluish.path import iterfiles
 from gluish.utils import shellout, random_string
 from siskin.sources.dbpedia import DBPGNDLinks, DBPDepictions
 from siskin.task import DefaultTask
@@ -138,6 +139,80 @@ class GNDNTriples(GNDTask):
 
     def output(self):
         return luigi.LocalTarget(path=self.path(ext='nt'))
+
+class GNDNTriplesSplitted(GNDTask):
+    """ Split Ntriples into chunks, so we see some progress in virtuoso. """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+    lines = luigi.IntParameter(default=1000000)
+
+    def requires(self):
+        return GNDNTriples(date=self.date)
+
+    @timed
+    def run(self):
+        prefix = '{0}-'.format(random_string())
+        output = shellout("cd {tmp} && split -l {lines} -a 8 {input} {prefix} && cd -",
+                          lines=self.lines, tmp=tempfile.gettempdir(),
+                          input=self.input().path, prefix=prefix)
+        target = os.path.join(self.taskdir())
+        if not os.path.exists(target):
+            os.makedirs(target)
+        with self.output().open('w') as output:
+            for path in iterfiles(tempfile.gettempdir()):
+                filename = os.path.basename(path)
+                if filename.startswith(prefix):
+                    dst = os.path.join(target, filename)
+                    shutil.move(path, dst)
+                    output.write_tsv(dst)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='filelist'), format=TSV)
+
+class GNDVirtuoso(GNDTask):
+    """ Load GND into virtuoso 6. Assume there is no graph yet.
+    Clear any graph manually with `SPARQL CLEAR GRAPH <http://d-nb.info/gnd/>;`
+
+    See SELECT * FROM DB.DBA.LOAD_LIST; for progress.
+
+    Also add `dirname $(taskoutput GNDNTriplesSplitted)` to
+    AllowedDirs in /etc/virtuoso-opensource-6.1/virtuoso.ini
+    """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+    graph = luigi.Parameter(default='http://d-nb.info/gnd/')
+    host = luigi.Parameter(default='localhost', significant=False)
+    port = luigi.IntParameter(default=1111, significant=False)
+    username = luigi.Parameter(default='dba', significant=False)
+    password = luigi.Parameter(default='dba', significant=False)
+
+    def requires(self):
+        return GNDNTriplesSplitted(date=self.date)
+
+    @timed
+    def run(self):
+        with self.input().open() as handle:
+            path = handle.iter_tsv(cols=('path',)).next().path
+            dirname = os.path.dirname(path)
+            filename = os.path.basename(path)
+            prefix, id = filename.split('-')
+
+        # allow access to all to dirname
+        os.chmod(dirname, 0777)
+
+        cmd = """
+            LD_DIR ('%s', '%s-*', '%s');
+            RDF_LOADER_RUN();
+        """ % (dirname, prefix, self.graph)
+        _, tmp = tempfile.mkstemp(prefix='siskin')
+        with open(tmp, 'w') as output:
+            output.write(cmd)
+        shellout("isql-vt {host}:{port} {username} {password} {file}",
+                 host=self.host, port=self.port, username=self.username, password=self.password, file=tmp)
+        shellout("touch {output}", output=self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='touch', digest=True))
 
 class GNDAbbreviatedNTriples(GNDTask):
     """ Get a Ntriples representation of GND, but abbreviate with ntto. """
