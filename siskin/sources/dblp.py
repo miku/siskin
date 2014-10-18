8f89fb24d5647ebbdefd148d6327cd4ed9c27128
Martin Czygan <martin.czygan@gmail.com>
2014-10-18 20:48:54 +0200

    added dblp source

---
+++ siskin/sources/dblp.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+from siskin.task import DefaultTask
+import datetime
+import luigi
+
+class DBLPTask(DefaultTask):
+    TAG = 'dblp'
+
+    def closest(self):
+        return monthly(date=self.date)
+
+class DBLPDownload(DefaultTask):
+    """ Download file and extract. """
+    url = luigi.Parameter(default='http://dblp.uni-trier.de/xml/dblp.xml.gz', significant=False)
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def run(self):
+        output = shellout("curl -L {url} | gunzip -c > {output}", url=self.url)
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='xml'))
