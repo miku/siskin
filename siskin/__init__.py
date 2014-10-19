69756c33cd19c7d1acb04a46f08e6076d4402118
Martin Czygan <martin.czygan@gmail.com>
2014-10-19 22:29:16 +0200

    v0.0.72

--- siskin/__init__.py
+++ siskin/__init__.py
@@ -12,7 +12,7 @@ import tempfile
 import warnings
 warnings.filterwarnings("ignore")
 
-__version__ = '0.0.71'
+__version__ = '0.0.72'
 
 config = Config.instance()
 tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
