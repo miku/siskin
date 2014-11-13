a6e779ea40ee5dffae374f151622978b3f6a8546
Martin Czygan <martin.czygan@gmail.com>
2014-11-13 01:06:16 +0100

    Update siskin_completion.sh

--- contrib/siskin_completion.sh
+++ contrib/siskin_completion.sh
@@ -5,13 +5,19 @@ _siskin_task_names()
 }
 
 complete -F _siskin_task_names taskcat
+complete -F _siskin_task_names taskdeps-dot
 complete -F _siskin_task_names taskdir
 complete -F _siskin_task_names taskdo
 complete -F _siskin_task_names taskdu
 complete -F _siskin_task_names taskfast
+complete -F _siskin_task_names taskhead
+complete -F _siskin_task_names taskhelp
 complete -F _siskin_task_names taskless
 complete -F _siskin_task_names taskls
+complete -F _siskin_task_names taskopen
 complete -F _siskin_task_names taskoutput
 complete -F _siskin_task_names taskredo
 complete -F _siskin_task_names taskrm
+complete -F _siskin_task_names taskstatus
+complete -F _siskin_task_names tasktree
 complete -F _siskin_task_names taskwc
