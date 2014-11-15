cb28848d7fa31a96868974f9f0dc591c8da97bb5
Martin Czygan <martin.czygan@gmail.com>
2014-11-15 17:07:09 +0100

    python 2.7 ships with importlib

--- setup.py
+++ setup.py
@@ -6,10 +6,49 @@ siskin is a set of tasks for library metadata management.
 ~~~~~~
 """
 
+import sys
+
 try:
     from setuptools import setup
 except:
     from distutils.core import setup
+
+install_requires = [
+    'BeautifulSoup==3.2.1',
+    'MySQL-python==1.2.5',
+    'argparse==1.2.1',
+    'astroid==1.1.1',
+    'colorama==0.2.7',
+    'decorator==3.4.0',
+    'elasticsearch==1.2.0',
+    'gluish==0.1.68',
+    'gspread==0.2.1',
+    'jsonpath-rw==1.3.0',
+    'logilab-common==0.61.0',
+    'luigi==1.0.19',
+    'lxml==3.3.5',
+    'marcx==0.1.17',
+    'nose==1.3.3',
+    'numpy==1.8.1',
+    'openpyxl==1.8.6',
+    'pandas==0.14.0',
+    'ply==3.4',
+    'prettytable==0.7.2',
+    'pyisbn==1.0.0',
+    'pylint==1.2.1',
+    'pymarc==3.0.1',
+    'python-dateutil==2.2',
+    'pytz==2014.4',
+    'requests==2.3.0',
+    'simplejson==3.6.0',
+    'six==1.6.1',
+    'sqlitebck==1.2.1',
+    'urllib3==1.8.2',
+    'wsgiref==0.1.2',
+]
+
+if sys.version_info < (2, 7):
+    install_requires.append('importlib==1.0.1')
 
 setup(name='siskin',
       version='0.0.84',
@@ -53,38 +92,5 @@ setup(name='siskin',
         'bin/taskversion',
         'bin/taskwc',
       ],
-      install_requires=[
-        'BeautifulSoup==3.2.1',
-        'MySQL-python==1.2.5',
-        'argparse==1.2.1',
-        'astroid==1.1.1',
-        'colorama==0.2.7',
-        'decorator==3.4.0',
-        'elasticsearch==1.2.0',
-        'gluish==0.1.68',
-        'gspread==0.2.1',
-        'importlib==1.0.1',
-        'jsonpath-rw==1.3.0',
-        'logilab-common==0.61.0',
-        'luigi==1.0.19',
-        'lxml==3.3.5',
-        'marcx==0.1.17',
-        'nose==1.3.3',
-        'numpy==1.8.1',
-        'openpyxl==1.8.6',
-        'pandas==0.14.0',
-        'ply==3.4',
-        'prettytable==0.7.2',
-        'pyisbn==1.0.0',
-        'pylint==1.2.1',
-        'pymarc==3.0.1',
-        'python-dateutil==2.2',
-        'pytz==2014.4',
-        'requests==2.3.0',
-        'simplejson==3.6.0',
-        'six==1.6.1',
-        'sqlitebck==1.2.1',
-        'urllib3==1.8.2',
-        'wsgiref==0.1.2',
-      ],
+      install_requires=install_requires,
 )
