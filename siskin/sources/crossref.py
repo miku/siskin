0bb9f3e67af42be8961a936a1968bde9f6ce1d86
Martin Czygan <martin.czygan@gmail.com>
2015-01-07 17:20:50 +0100

    crossref: a tiny bit faster and more robust

--- siskin/sources/crossref.py
+++ siskin/sources/crossref.py
@@ -38,18 +38,22 @@ class CrossrefHarvestChunk(CrossrefTask):
     """
     begin = luigi.DateParameter()
     end = luigi.DateParameter()
-    rows = luigi.IntParameter(default=100, significant=False)
+    rows = luigi.IntParameter(default=1000, significant=False)
+    max_retries = luigi.IntParameter(default=10, significant=False)
 
     def run(self):
-        if self.rows < 1:
-            raise RuntimeError('rows parameter must be positive')
+        sess = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(max_retries=self.max_retries)
+        sess.mount('http://', adapter)
+
         filter = "from-index-date:%s,until-index-date:%s" % (str(self.begin), str(self.end))
         rows, offset = self.rows, 0
+
         with self.output().open('w') as output:
             while True:
                 params = {"rows": rows, "offset": offset, "filter": filter}
                 url = "http://api.crossref.org/works?%s" % urllib.urlencode(params)
-                r = requests.get(url)
+                r = sess.get(url)
                 if r.status_code == 200:
                     content = json.loads(r.text)
                     items = content["message"]["items"]
@@ -71,7 +75,7 @@ class CrossrefHarvest(luigi.WrapperTask, CrossrefTask):
     """
     begin = luigi.DateParameter(default=datetime.date(1970, 1, 1))
     end = luigi.DateParameter()
-    rows = luigi.IntParameter(default=100, significant=False)
+    rows = luigi.IntParameter(default=1000, significant=False)
 
     def requires(self):
         dates = date_range(self.begin, self.end, 1, 'months')
@@ -89,7 +93,7 @@ class CrossrefCombine(CrossrefTask):
     """
     begin = luigi.DateParameter(default=datetime.date(1970, 1, 1))
     date = ClosestDateParameter(default=datetime.date.today())
-    rows = luigi.IntParameter(default=100, significant=False)
+    rows = luigi.IntParameter(default=1000, significant=False)
 
     def requires(self):
         return CrossrefHarvest(begin=self.begin, end=self.closest(), rows=self.rows)
