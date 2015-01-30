949989b62d698a5e6f6a47a498c77fd74f8bfaa5
Martin Czygan <martin.czygan@gmail.com>
2015-01-30 18:16:18 +0100

    v0.0.93

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if sys.version_info < (2, 7):
     install_requires.append('importlib==1.0.2')
 
 setup(name='siskin',
-      version='0.0.91',
+      version='0.0.93',
       description='Various sources and workflows.',
       url='https://github.com/miku/siskin',
       author='Martin Czygan',
