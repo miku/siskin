24e89955a43251df0c88c0c3cbc8842605fdc93a
Martin Czygan <martin.czygan@gmail.com>
2015-02-26 16:10:44 +0100

    add DegruyterXMLFiles task and combinator stub

--- siskin/sources/degruyter.py
+++ siskin/sources/degruyter.py
@@ -17,9 +17,11 @@ from gluish.benchmark import timed
 from gluish.common import FTPMirror
 from gluish.format import TSV
 from gluish.intervals import daily
+from gluish.utils import shellout
 from siskin.configuration import Config
 from siskin.task import DefaultTask
 import luigi
+import tempfile
 
 config = Config.instance()
 
@@ -28,7 +30,7 @@ class DegruyterTask(DefaultTask):
 
 class DegruyterPaths(DegruyterTask):
     """ A list of Degruyter ile paths (via FTP). """
-    indicator = luigi.Parameter(default=daily(fmt='%s'))
+    indicator = luigi.Parameter(default=daily().strftime('%s'))
 
     def requires(self):
         host = config.get('degruyter', 'ftp-host')
@@ -45,3 +47,37 @@ class DegruyterPaths(DegruyterTask):
 
     def output(self):
         return luigi.LocalTarget(path=self.path(ext="filelist"), format=TSV)
+
+class DegruyterXMLFiles(DegruyterTask):
+    """ Extract all XML files from Degruyter dump. """
+
+    def requires(self):
+        return DegruyterPaths()
+
+    @timed
+    def run(self):
+        stopover = tempfile.mkdtemp(prefix='siskin-')
+        with self.input().open() as handle:
+            for row in handle.iter_tsv(cols=('path',)):
+                shellout("unzip -qq -d {stopover} {path} *.xml 2> /dev/null", stopover=stopover, path=row.path,
+                         ignoremap={1: '... but processing completed successfully anyway'})
+        shutil.move(stopover, self.taskdir())
+        with self.output().open('w') as output:
+            for path in iterfiles(self.taskdir(), fun=lambda p: p.endswith('.xml')):
+                output.write_tsv(path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="filelist"), format=TSV)
+
+class DegruyterXMLCombine(DegruyterTask):
+    """ Synthesize a single XML file from all article XML files. """
+
+    def requires(self):
+        return DegruyterXMLFiles()
+
+    @timed
+    def run(self):
+        pass
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="xml"))
