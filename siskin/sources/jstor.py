534b5b9b2a12c6870975d3e16dbbf68af995b85d
Martin Czygan <martin.czygan@gmail.com>
2015-03-25 10:27:46 +0100

    jstor: xml handling

--- siskin/sources/jstor.py
+++ siskin/sources/jstor.py
@@ -10,13 +10,20 @@ ftp-path = /
 ftp-pattern = *
 """
 
+from gluish.benchmark import timed
 from gluish.common import FTPMirror
-from gluish.intervals import daily
+from gluish.format import TSV
+from gluish.intervals import weekly
 from gluish.parameter import ClosestDateParameter
-from siskin.task import DefaultTask
+from gluish.path import iterfiles
+from gluish.utils import shellout
 from siskin.configuration import Config
+from siskin.task import DefaultTask
 import datetime
 import luigi
+import re
+import shutil
+import tempfile
 
 config = Config.instance()
 
@@ -25,9 +32,9 @@ class JstorTask(DefaultTask):
     TAG = 'jstor'
 
     def closest(self):
-        return daily(self.date)
+        return weekly(self.date)
 
-class JstorSync(JstorTask):
+class JstorPaths(JstorTask):
     """ Sync. """
     date = ClosestDateParameter(default=datetime.date.today())
 
@@ -41,4 +48,50 @@ class JstorSync(JstorTask):
         self.input().move(self.output().path)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path())
+        return luigi.LocalTarget(path=self.path(), format=TSV)
+
+class JstoreXMLFiles(JstorTask):
+    """ Extract all XML files from Jstor dump. """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return JstorPaths(date=self.date)
+
+    @timed
+    def run(self):
+        stopover = tempfile.mkdtemp(prefix='siskin-')
+        with self.input().open() as handle:
+            for row in handle.iter_tsv(cols=('path',)):
+                shellout("unzip -qq -d {stopover} {path} *.xml 2> /dev/null", stopover=stopover, path=row.path, ignoremap={1: 'OK'})
+        shutil.move(stopover, self.taskdir())
+        with self.output().open('w') as output:
+            for path in iterfiles(self.taskdir(), fun=lambda p: p.endswith('.xml')):
+                output.write_tsv(path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="filelist"), format=TSV)
+
+class JstoreXMLCombine(JstorTask):
+    """ Synthesize a single XML file from all article XML files. """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return JstoreXMLFiles(date=self.date)
+
+    @timed
+    def run(self):
+        docp = re.compile(r"<!DOCTYPE[^>]*>", re.IGNORECASE | re.MULTILINE)
+        decp = re.compile(r"<\?xml[^>]*>", re.IGNORECASE | re.MULTILINE)
+        with self.output().open('w') as output:
+            output.write('<synthetic-root>')
+            with self.input().open() as handle:
+                for row in handle.iter_tsv(cols=('path',)):
+                    with open(row.path) as fh:
+                        content = fh.read()
+                        content = docp.sub("", content)
+                        content = decp.sub("", content)
+                        output.write(content)
+            output.write('</synthetic-root>')
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="xml"))
