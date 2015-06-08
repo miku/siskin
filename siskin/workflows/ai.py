af106c5edef94b355c4b69c44f5a7d9f8428ddcc
Martin Czygan <martin.czygan@gmail.com>
2015-06-08 14:48:03 +0200

    ai: import and export stub

--- siskin/workflows/ai.py
+++ siskin/workflows/ai.py
@@ -18,6 +18,18 @@ class AITask(DefaultTask):
     def closest(self):
         return weekly(self.date)
 
+class DownloadFile(AITask):
+    """ Download a file. TODO(miku): move this out here. """
+
+    date = luigi.DateParameter(default=datetime.date.today())
+    url = luigi.Parameter()
+
+    def run(self):
+        output = shellout("""curl "{url}" > {output}""", url=url)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(digest=True))
+
 class AIIntermediateSchema(AITask):
     """ Create an intermediate schema record from all AI sources. """
 
@@ -34,8 +46,36 @@ class AIIntermediateSchema(AITask):
         }
 
     def run(self):
-        for name, target in self.input().iteritems():
+        _, stopover = tempfile.mkdtemp(prefix='siskin-')
+        for format, target in self.input().iteritems():
+            if name == "_":
+                continue
+            shellout("span -i {format} {input} >> {output}", format=format, input=target.path, output=stopover)
+        luigi.File(stopover).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='ldj'))
+
+class AIImportable(AITask):
+    """ Create target, that is importable into SOLR. """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return {
+            'input': AIIntermediateSchema(date=self.date),
+            'DE-15': HoldingsFile(date=self.date, isil='DE-15'),
+            'DE-14': HoldingsFile(date=self.date, isil='DE-14'),
+            'DE-105': HoldingsFile(date=self.date, isil='DE-105'),
+            'DE-Ch1': HoldingsFile(date=self.date, isil='DE-Ch1'),
+            'DE-Gla1': HoldingsFile(date=self.date, isil='DE-Gla1'),
+            'DE-Bn3': HoldingsFile(date=self.date, isil='DE-Bn3'),
+            'DE-15-FID': DownloadFile(date=self.date, url='https://goo.gl/8P6JtB')
+        }
+
+    def run(self):
+        for name, target in self.input():
             print(name, target)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path())
+        return luigi.LocalTarget(path=self.path(ext='ldj'))
