86551c9a78c2c0b4ba271a654ea23ec16818c96b
Martin Czygan <martin.czygan@gmail.com>
2015-06-20 14:29:56 +0200

    jstor: find latest articles fast

--- siskin/sources/jstor.py
+++ siskin/sources/jstor.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,R0904,E1103,C0301
 
 """
 [jstor]
@@ -15,15 +16,13 @@ from gluish.common import FTPMirror, Executable
 from gluish.format import TSV
 from gluish.intervals import weekly
 from gluish.parameter import ClosestDateParameter
-from gluish.path import iterfiles
-from gluish.utils import shellout
+from gluish.utils import shellout, nwise
 from siskin.configuration import Config
 from siskin.task import DefaultTask
-import collections
 import datetime
+import itertools
 import luigi
-import re
-import shutil
+import pipes
 import tempfile
 
 config = Config.instance()
@@ -51,8 +50,8 @@ class JstorPaths(JstorTask):
     def output(self):
         return luigi.LocalTarget(path=self.path(), format=TSV)
 
-class JstorJournals(JstorTask):
-    """ WIP: Group Jstor data by journal. """
+class JstorMembers(JstorTask):
+    """ Extract a full list of archive members. """
     date = ClosestDateParameter(default=datetime.date.today())
 
     def requires(self):
@@ -70,50 +69,46 @@ class JstorJournals(JstorTask):
     def output(self):
         return luigi.LocalTarget(path=self.path(), format=TSV)
 
-class JstorLatest(JstorTask):
-    """
-    Get a list of latest Jstor article versions.
-    TODO(miku): make this faster by extracting all relevant members from an archive at once.
-    """
+class JstorLatestMembers(JstorTask):
+    """ Find the latest archive members for all article ids. """
     date = ClosestDateParameter(default=datetime.date.today())
 
     def requires(self):
-        return JstorJournals(date=self.date)
+        return JstorMembers(date=self.date)
 
     @timed
     def run(self):
         """ Expect input to be sorted, so tac will actually be a perfect rewind. """
-        output = shellout("tac {input} | sort -S 50% -u -k2,2 > {output}", input=self.input().path)
-
-        _, stopover = tempfile.mkstemp(prefix='siskin-')
-        with luigi.File(output, format=TSV).open() as handle:
-            for row in handle.iter_tsv(cols=('archive', 'member')):
-                shellout("unzip -qq -c {archive} {member} >> {output}",
-                         archive=row.archive, member=row.member, output=stopover)
-
-        luigi.File(stopover).move(self.output().path)
+        output = shellout("tac {input} | sort -S 50% -u -k2,2 | sort -S 50% -k1,1 > {output}", input=self.input().path)
+        luigi.File(output).move(self.output().path)
 
     def output(self):
         return luigi.LocalTarget(path=self.path(), format=TSV)
 
 class JstorXML(JstorTask):
-    """ Extract all XML files from Jstor dump. """
+    """
+    Create an latest snapshot of the latest data.
+    """
     date = ClosestDateParameter(default=datetime.date.today())
+    batch = luigi.IntParameter(default=4096, significant=False)
 
     def requires(self):
-        return JstorPaths(date=self.date)
+        return JstorLatestMembers(date=self.date)
 
     @timed
     def run(self):
         _, stopover = tempfile.mkstemp(prefix='siskin-')
         with self.input().open() as handle:
-            for row in handle.iter_tsv(cols=('path',)):
-                shellout("unzip -p {path} \*.xml 2> /dev/null >> {output}", output=stopover, path=row.path,
-                         ignoremap={1: 'OK', 9: 'skipping broken zipfile'})
+            groups = itertools.groupby(handle.iter_tsv(cols=('archive', 'member')), lambda row: row.archive)
+            for archive, items in groups:
+                for chunk in nwise(items, n=self.batch):
+                    margs = " ".join(["'%s'" % item.member.replace('[', r'\[').replace(']', r'\]') for item in chunk])
+                    shellout("unzip -qq -c {archive} {members} >> {output}", archive=archive, members=margs, output=stopover)
+
         luigi.File(stopover).move(self.output().path)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path(ext='xml'), format=TSV)
+        return luigi.LocalTarget(path=self.path(), format=TSV)
 
 class JstorIntermediateSchema(JstorTask):
     """ Convert to intermediate format via span. """
