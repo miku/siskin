c3edb421a2de2494fa06faeb36df0146db7b7f8a
Martin Czygan <martin.czygan@gmail.com>
2015-08-01 12:26:09 +0200

    doi: add DOI blacklist task

--- siskin/sources/doi.py
+++ siskin/sources/doi.py
@@ -44,3 +44,27 @@ class DOICheck(DOITask):
 
     def output(self):
         return luigi.LocalTarget(path=self.path(ext='tsv.gz'))
+
+class DOIBlacklist(DOITask):
+    """
+    Create a blacklist of DOIs. Possible cases:
+
+    1. A DOI redirects to http://www.crossref.org/deleted_DOI.html
+    2. A DOI API lookup results in a HTTP 4XX.
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return DOICheck(date=self.date)
+
+    def run(self):
+        _, stopover = tempfile.mkstemp(prefix='siskin-')
+        shellout("""LC_ALL=C zgrep -F "http://www.crossref.org/deleted_DOI.html" {input} >> {output}""",
+                 input=self.input().path, output=stopover)
+        shellout("""LC_ALL=C zgrep -v "^200" {input} >> {output}""",
+                 input=self.input().path, output=stopover)
+        output = shellout("sort -S50% -u {input} > {output}", input=stopover)
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
