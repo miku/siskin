fe2bfba83aa632753ef26bb7c9342a19cb8b9515
Martin Czygan <martin.czygan@gmail.com>
2015-08-02 16:09:22 +0200

    completion: do not panic on missing tasknames command

--- contrib/siskin_completion.sh
+++ contrib/siskin_completion.sh
@@ -1,5 +1,9 @@
+# siskin task name completion
+#
+# TODO(miku): autocomplete task parameters, too
 _siskin_task_names()
 {
+    hash tasknames 2>/dev/null || { return 1; }
     local cur=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=( $(compgen -W "$(tasknames)" -- $cur) )
 }
