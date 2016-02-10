1abf72da408776d81f175807d1ff6802efc1ddcf
Martin Czygan <martin.czygan@gmail.com>
2016-02-10 10:18:22 +0100

    use github api to get latest version

--- install.sh
+++ install.sh
@@ -14,24 +14,55 @@ if [[ "$PYVER" != "2.7" ]]; then
 fi
 
 hash pip 2> /dev/null || { echo >&2 "pip is required."; exit 1; }
+hash jq 2> /dev/null || { echo >&2 "jq is required."; exit 1; }
+hash curl 2> /dev/null || { echo >&2 "curl is required."; exit 1; }
 
 pip install -U siskin
 
 echo "Installing command line tools..."
 
+# install_latest_deb installs latest deb, given a username/repository on github.com.
+install_latest_deb() {
+    if [ $# -eq 0 ]; then
+        echo "latest_deb_url expects an argument"
+        exit 1
+    fi
+    URL=$(curl -s https://api.github.com/repos/$1/releases | jq '.[0].assets_url' | xargs curl -s | jq -r '.[].browser_download_url' | grep "deb")
+    curl -o- "$URL" | dpkg --install
+}
+
+# install_latest_rpm rinstalls latest rpm, given a username/repository on github.com.
+install_latest_rpm() {
+    if [ $# -eq 0 ]; then
+        echo "latest_rpm_url expects an argument"
+        exit 1
+    fi
+    URL=$(curl -s https://api.github.com/repos/$1/releases | jq '.[0].assets_url' | xargs curl -s | jq -r '.[].browser_download_url' | grep "rpm")
+    yum install -y "$URL"
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [ -f /etc/debian_version ]; then
         echo "Usings debs."
+
+        install_latest_deb "miku/span"
+        install_latest_deb "miku/solrbulk"
+        install_latest_deb "miku/memcldj"
+        install_latest_deb "miku/hurrly"
+        install_latest_deb "miku/esbulk"
+        install_latest_deb "miku/oaimi"
+
     elif [ -f /etc/redhat-release ]; then
         echo "Usings rpms."
 
         yum install -y jq xmlstarlet lftp vim tmux bash-completion tree
-        yum install -y https://github.com/miku/esbulk/releases/download/v0.3.5/esbulk-0.3.5-0.x86_64.rpm
-        yum install -y https://github.com/miku/filterline/releases/download/v0.1.3/filterline-0.1.3-0.x86_64.rpm
-        yum install -y https://github.com/miku/hurrly/releases/download/v0.1.4/hurrly-0.1.4-0.x86_64.rpm
-        yum install -y https://github.com/miku/memcldj/releases/download/v1.3.0/memcldj-1.3.0-0.x86_64.rpm
-        yum install -y https://github.com/miku/solrbulk/releases/download/v0.1.5.4/solrbulk-0.1.5.4-0.x86_64.rpm
-        yum install -y https://github.com/miku/span/releases/download/v0.1.59/span-0.1.59-0.x86_64.rpm
+
+        install_latest_rpm "miku/span"
+        install_latest_rpm "miku/solrbulk"
+        install_latest_rpm "miku/memcldj"
+        install_latest_rpm "miku/hurrly"
+        install_latest_rpm "miku/esbulk"
+        install_latest_rpm "miku/oaimi"
     else
         echo "Using binaries."
     fi
