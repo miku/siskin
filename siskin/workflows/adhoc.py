b89040ad81447c55defa6b592fd4f445b4a28576
Martin Czygan <martin.czygan@gmail.com>
2016-03-02 15:29:45 +0100

    adhoc: simplify CrossrefWebOfScienceMatch

--- siskin/workflows/adhoc.py
+++ siskin/workflows/adhoc.py
@@ -81,39 +81,39 @@ class DOIList(AdhocTask):
 class CrossrefWebOfScienceMatch(AdhocTask):
     """
     Find all ISSNs for Web of Science export.
+
+    The input --file must contain one DOI per line:
+
+    005410.001002/14651858.CD14005465.pub14651852
+    10.1002/ange.201106065
+    10.1007/BFb0061839.56
+    ..
     """
-    file = luigi.Parameter(description='web of science csv')
+    date = luigi.DateParameter(default=datetime.date.today())
+    file = luigi.Parameter(description='web of science doi list', significant=False)
 
     def requires(self):
-        return CrossrefDOIAndISSNList()
+        return CrossrefDOIAndISSNList(date=self.date)
 
     def run(self):
-        wos = set()
+        # wos contains all WOS DOIs, found keeps the DOIs, that could be found in Crossref
+        wos, found = set(), set()
 
         with open(self.file) as handle:
             for line in handle:
                 wos.add(line.strip().lower())
 
-        found = set()
-
         with self.input().open() as handle:
-
             with self.output().open('w') as output:
-                pass
-
-            with open(self.output().path, 'wb') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                 for line in handle:
                     parts = [s.strip('"') for s in line.strip().split(',')]
                     doi, issns = parts[0].lower(), parts[1:]
                     if doi in wos:
-                        csvwriter.writerow(['OK', doi] + issns)
-                        # output.write_tsv('OK', doi, *issns)
+                        output.write_tsv('OK', doi, *issns)
                         found.add(doi)
 
                 for id in wos.difference(found):
-                    csvwriter.writerow(['NOT_FOUND', id])
-                    # output.write_tsv('NOT_FOUND', id)
+                    output.write_tsv('NOT_FOUND', id)
 
     def output(self):
         return luigi.LocalTarget(path=self.path(), format=TSV)
