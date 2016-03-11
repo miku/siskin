31d6307f94407e0b630f5c2f05697ee7e370e3fe
Martin Czygan <martin.czygan@gmail.com>
2016-03-11 15:56:38 +0100

    wos: move CrossrefWebOfScienceMatch -> WOSCrossrefMatch

--- siskin/sources/wos.py
+++ siskin/sources/wos.py
@@ -6,6 +6,7 @@ Web of Science.
 
 from gluish.utils import shellout
 from siskin.task import DefaultTask
+from siskin.sources.crossref import CrossrefDOIAndISSNList
 from gluish.format import TSV
 import datetime
 import luigi
@@ -19,7 +20,7 @@ class WOSTask(DefaultTask):
 
 class WOSPublicationList(WOSTask):
     """
-    Publication list.
+    Publication list. PDF -> txt.
     """
     date = luigi.Parameter(default=datetime.date.today())
 
@@ -50,6 +51,46 @@ class WOSISSNList(WOSTask):
         with self.output().open('w') as output:
             for issn in sorted(issns):
                 output.write_tsv(issn)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(), format=TSV)
+
+class WOSCrossrefMatch(WOSTask):
+    """
+    Find all ISSNs for Web of Science export.
+
+    The input --file must contain one DOI per line:
+
+    005410.001002/14651858.CD14005465.pub14651852
+    10.1002/ange.201106065
+    10.1007/BFb0061839.56
+    ..
+    """
+    date = luigi.DateParameter(default=datetime.date.today())
+    file = luigi.Parameter(description='web of science doi list', significant=False)
+
+    def requires(self):
+        return CrossrefDOIAndISSNList(date=self.date)
+
+    def run(self):
+        # wos contains all WOS DOIs, found keeps the DOIs, that could be found in Crossref
+        wos, found = set(), set()
+
+        with open(self.file) as handle:
+            for line in handle:
+                wos.add(line.strip().lower())
+
+        with self.input().open() as handle:
+            with self.output().open('w') as output:
+                for line in handle:
+                    parts = [s.strip('"') for s in line.strip().split(',')]
+                    doi, issns = parts[0].lower(), parts[1:]
+                    if doi in wos:
+                        output.write_tsv('OK', doi, *issns)
+                        found.add(doi)
+
+                for id in wos.difference(found):
+                    output.write_tsv('NOT_FOUND', id)
 
     def output(self):
         return luigi.LocalTarget(path=self.path(), format=TSV)
