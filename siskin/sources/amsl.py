e048b9d5676b33e74f1d2e612a5de79e6016b656
Martin Czygan <martin.czygan@gmail.com>
2016-03-13 13:06:11 +0100

    amsl: possible api change

--- siskin/sources/amsl.py
+++ siskin/sources/amsl.py
@@ -147,7 +147,7 @@ class AMSLHoldingsFile(AMSLTask):
         _, stopover = tempfile.mkstemp(prefix='siskin-')
 
         # The property which contains the URI of the holding file. Might change.
-        urikey = 'DokumentURI'
+        urikey = 'HoldingFileURI'
 
         for holding in holdings:
             if holding["ISIL"] == self.isil:
