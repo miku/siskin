1f93fefb1f35b0e36d1504825192b45752b07301
Martin Czygan <martin.czygan@gmail.com>
2016-08-11 15:26:38 +0200

    dummy: tasks for playing and testing

---
+++ siskin/sources/dummy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Dummy module for testing out tasks.
+"""
+
+import datetime
+from siskin.task import DefaultTask
+
+class DummyTask(DefaultTask):
+    TAG = 'dummy'
+
+class DummyHelloWorld(DummyTask):
+    """
+    Just say hello world.
+    """
+    def run(self):
+        print("Hello from %s" % self.__class__.__name__)
+
+    def complete(self):
+        return False
+
+class DummyFail(DummyTask):
+    """
+    A task that always fails.
+    """
+
+    def run(self):
+        raise RuntimeError('This error occured at %s' % datetime.datetime.now())
+
+    def complete(self):
+        return False
