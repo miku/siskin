cea3fc3743d903d215e5ab73e172782916bfacac
Martin Czygan <martin.czygan@gmail.com>
2016-11-03 14:21:07 +0100

    mhlibrary: example workflow with oai and metafacture

---
+++ siskin/assets/103/103.flux
@@ -0,0 +1,20 @@
+// liest XML ein (Trennung der einzelnen Records durch "Record"), uebergibt an morph und speichert in json
+
+// die default Werte werden beim Aufruf des Skripts �berschrieben
+default MAP_DIR ="/home/tracy/maps/";
+default sid = "103";
+default mega_collection = "Margaret Herrick Library";
+// default fileName = FLUX_DIR + "sid" + sid + "records.xml";
+// default out = FLUX_DIR + "sid" + sid + ".json";
+default fileName = in;
+
+fileName|
+open-file|
+decode-xml|
+handle-generic-xml("Record")|
+// morph(FLUX_DIR + "sid" + sid + "morph.xml", *)|
+filter(FLUX_DIR + sid + "filter.xml")|
+morph(FLUX_DIR + sid + "_morph.xml", *)|
+//encode-json(prettyprinting="true")|
+encode-json|
+write("stdout");
---
+++ siskin/sources/mhlibrary.py
@@ -0,0 +1,73 @@
+# coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,R0904,E1103,C0301
+
+# Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+Example workflow with OAI harvest and metafacture.
+"""
+
+from gluish.intervals import monthly
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+from siskin.task import DefaultTask
+import datetime
+import luigi
+
+class MHLibraryTask(DefaultTask):
+    TAG = '103'
+
+    def closest(self):
+        return monthly(date=self.date)
+
+class MHLibraryHarvest(MHLibraryTask):
+    """
+    Harvest.
+    """
+    endpoint = luigi.Parameter(default='http://cdm15759.contentdm.oclc.org/oai/oai.php', significant=False)
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def run(self):
+        shellout("metha-sync ")
+        output = shellout("""metha-cat -root Records "{endpoint}" > {output}""", endpoint=self.endpoint)
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='xml'))
+
+class MHLibraryIntermediateSchema(MHLibraryTask):
+    """
+    Convert to intermediate schema via metafacture. Custom morphs, flux and maps are kept in assets/103.
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return MHLibraryHarvest(date=self.date)
+
+    def run(self):
+        mapdir = 'file:///%s' % self.assets("103/")
+        output = shellout("""flux.sh {flux} in={input} MAP_DIR={mapdir} > {output}""",
+                          flux=self.assets("103/103.flux"), mapdir=mapdir, input=self.input().path)
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='ldj'))
