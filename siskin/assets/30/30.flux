eabd5885a3a4acfdeb21d4d66cce198c88b5a888
Tracy <hoffmannt@ub.uni-leipzig.de>
2016-12-05 14:33:09 +0100

    Add SSOAR workflow

---
+++ siskin/assets/30/30.flux
@@ -0,0 +1,20 @@
+// read MARC21XML
+// morph to intermediate schema
+
+// SID: 103
+
+// override default values if necessary
+default MAP_DIR ="/assets/maps/";
+default sid = "30"; 
+default mega_collection = "SSOAR";
+default fileName = in;
+//default fileName = FLUX_DIR + "marcxml.xml";
+//default out = FLUX_DIR + sid + ".json";
+
+fileName|
+open-file|
+decode-xml|
+handle-marcxml|
+morph(FLUX_DIR + sid + "_morph.xml", *)|
+encode-json|
+write("stdout");
---
+++ siskin/sources/ssoar.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,R0904,E1103,C0301
+
+# Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+SSOAR workflow with OAI harvest and metafacture.
+"""
+
+import datetime
+
+import luigi
+from gluish.intervals import monthly
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+
+from siskin.task import DefaultTask
+
+
+class SSOARTask(DefaultTask):
+    TAG = '30'
+
+    def closest(self):
+        return monthly(date=self.date)
+
+class SSOARHarvest(SSOARTask):
+    """
+    Harvest.
+    """
+    format = luigi.Parameter(default="marcxml", significant=False)
+    endpoint = luigi.Parameter(default='http://www.ssoar.info/OAIHandler/request', significant=False)
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def run(self):
+        shellout("""metha-sync -format {format} {endpoint} """, endpoint=self.endpoint,
+                format=self.format)
+        output = shellout("""metha-cat -format {format} -root Records {endpoint} > {output}""", endpoint=self.endpoint,
+                          format=self.format)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='xml'))
+
+class SSOARIntermediateSchema(SSOARTask):
+    """
+    Convert to intermediate schema via metafacture. Custom morphs and flux are kept in assets/30.
+    Maps are kept in assets/maps
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return SSOARHarvest(date=self.date)
+
+    def run(self):
+        mapdir = 'file:///%s' % self.assets("maps/")
+        output = shellout("""flux.sh {flux} in={input} MAP_DIR={mapdir} > {output}""",
+                          flux=self.assets("30/30.flux"), mapdir=mapdir, input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='ldj'))
