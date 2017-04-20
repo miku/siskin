d15e83f5bcb9b65f058bff2074d5d386c7c1f8bc
Martin Czygan <martin.czygan@gmail.com>
2017-04-20 11:38:23 +0200

    ai: diff errs with memory exhausted

--- siskin/workflows/ai.py
+++ siskin/workflows/ai.py
@@ -530,7 +530,7 @@ class AICompareLicensing(AITask):
 
         filemap = dict([queue.get() for _ in processes])
 
-        output = shellout("diff {current} {next} > {output}",
+        output = shellout("diff -q -H {current} {next} > {output}",
                           current=filemap.get('current'), next=filemap.get('next'))
         luigi.LocalTarget(output).move(self.output().path)
 
