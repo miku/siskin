ac258368013a5803a592170db84cb0d1d3f231f6
Robert Schenk <schenk@ub.uni-leipzig.de>
2017-05-11 14:36:46 +0200

    fixed joins, refs #5246

--- siskin/assets/70/query.sql
+++ siskin/assets/70/query.sql
@@ -1,27 +1,38 @@
 SELECT 
-            ReferenceID,
-            ReferenceType,
-            Reference.Title as Title,
-            SubTitle,
-            GROUP_CONCAT(substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0), ';') as Person,
-            Periodical.Name as Journal,
-	        Publisher.Name as Publisher,
-	        Language,
-	        Volume,
-	        Number as Issue,
-	        CustomField4 as Place,
-	        CustomField3 as Year,
-	        StartPage,
-	        EndPage,
-	        ISBN,
-			GROUP_CONCAT(CallNumber , "; " ) as Signatur
-        FROM
-			Reference
-		LEFT JOIN ReferenceAuthor USING (ReferenceID)
-		LEFT JOIN Person USING (PersonID)
-		LEFT JOIN ReferencePublisher USING (ReferenceID)
-		LEFT JOIN Publisher USING (PublisherID)
-		LEFT JOIN Periodical USING (PeriodicalID)
-		LEFT JOIN Location USING (ReferenceID)		
-		GROUP BY
-			Reference.ReferenceID
+	Reference.ID as ReferenceID,
+	ReferenceType,
+	Reference.Title as Title,
+	SubTitle,
+	GROUP_CONCAT(substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0), ';') as Person,
+	Periodical.Name as Journal,
+	Publisher.Name as Publisher,
+	Language,
+	Volume,
+	Number as Issue,
+	CustomField4 as Place,
+	CustomField3 as Year,
+	StartPage,
+	EndPage,
+	ISBN,
+	Signatur
+FROM	
+	Reference
+LEFT JOIN
+	ReferenceAuthor ON (ReferenceAuthor.ReferenceID = Reference.ID)
+LEFT JOIN
+	Person ON (Person.ID = ReferenceAuthor.PersonID)
+LEFT JOIN
+	ReferencePublisher ON (ReferencePublisher.ReferenceID = Reference.ID)
+LEFT JOIN
+	Publisher ON (Publisher.ID = ReferencePublisher.PublisherID)
+LEFT JOIN
+	Periodical ON (Periodical.ID = Reference.PeriodicalID)
+LEFT JOIN
+	(SELECT
+		GROUP_CONCAT(CallNumber , "; " ) AS Signatur,
+		ReferenceID
+	FROM
+		Location
+	GROUP BY ReferenceID) AS Location2 ON (Location2.ReferenceID = Reference.ID) 
+GROUP BY
+	Reference.ID
