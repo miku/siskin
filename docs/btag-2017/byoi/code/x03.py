d1086c6a337e7c30c10e10b75aecf6b20b7300a4
Martin Czygan <martin.czygan@gmail.com>
2017-05-28 18:35:04 +0200

    btag: x03, requires

---
+++ docs/btag-2017/byoi/code/x03.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+A first dependency.
+"""
+
+import json
+import luigi
+
+
+class IPlannerResponse(luigi.ExternalTask):
+    """
+    A response from http://www.professionalabstracts.com API.
+    """
+
+    def output(self):
+	return luigi.LocalTarget(path='inputs/iplanner.json')
+
+
+class Welcome(luigi.Task):
+    """
+    A welcome message written to a file.
+    """
+
+    def requires(self):
+	return IPlannerResponse()
+
+    def run(self):
+	with self.input().open() as file:
+	    doc = json.load(file)
+
+	with self.output().open('w') as output:
+	    output.write("Willkommen zum Hands-On Lab '%s' am DBT!\n" % doc['sessions']['1']['title'])
+
+    def output(self):
+	return luigi.LocalTarget(path='x03.output.txt')
+
+if __name__ == '__main__':
+    luigi.run(local_scheduler=True)
