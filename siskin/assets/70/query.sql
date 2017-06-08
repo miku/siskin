94641345221460ad9ec64ff0f3ddd3f3d3d83a61
Robert Schenk <schenk@ub.uni-leipzig.de>
2017-06-08 15:54:36 +0200

    update sql, refs #5246

--- siskin/assets/70/query.sql
+++ siskin/assets/70/query.sql
@@ -1,26 +1,40 @@
 SELECT 
-	Reference.ID as ReferenceID,
-	ReferenceType,
-	Reference.Title as Title,
-	SubTitle,
-	GROUP_CONCAT(substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0), ';') as Person,
-	Periodical.Name as Journal,
-	Publisher.Name as Publisher,
-	Language,
-	Volume,
-	Number as Issue,
-	CustomField4 as Place,
-	CustomField3 as Year,
-	StartPage,
-	EndPage,
-	ISBN,
-	Signatur
-FROM	
-	Reference
+    Reference.ID as ReferenceID,
+    ReferenceType,
+    Reference.Title as Title,
+    SubTitle,
+    Periodical.Name as Journal,
+    Publisher.Name as Publisher,
+    Language,
+    Volume,
+    Number as Issue,
+    CustomField4 as Place,
+    CustomField3 as Year,
+    StartPage,
+    EndPage,
+    ISBN,
+    Signatur,
+	GROUP_CONCAT(Author , "; " ) As Author,
+	GROUP_CONCAT(Editor , "; " ) As Editor,
+	SeriesTitle.Name as Series
+FROM
+    Reference 
 LEFT JOIN
 	ReferenceAuthor ON (ReferenceAuthor.ReferenceID = Reference.ID)
 LEFT JOIN
-	Person ON (Person.ID = ReferenceAuthor.PersonID)
+	(SELECT
+		ID as AuthorID,
+		substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Author
+	FROM
+		Person) ON (AuthorID = ReferenceAuthor.PersonID)
+LEFT JOIN
+	ReferenceEditor ON (ReferenceEditor.ReferenceID = Reference.ID)
+LEFT JOIN
+	(SELECT
+		ID as EditorID,
+		substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Editor
+	FROM
+		Person) ON (EditorID = ReferenceEditor.PersonID)
 LEFT JOIN
 	ReferencePublisher ON (ReferencePublisher.ReferenceID = Reference.ID)
 LEFT JOIN
@@ -33,6 +47,9 @@ LEFT JOIN
 		ReferenceID
 	FROM
 		Location
-	GROUP BY ReferenceID) AS Location2 ON (Location2.ReferenceID = Reference.ID) 
+	GROUP BY
+		ReferenceID) as Location2 ON (Location2.ReferenceID = Reference.ID)
+LEFT JOIN
+	SeriesTitle ON (SeriesTitle.ID = Reference.SeriesTitleID)
 GROUP BY
 	Reference.ID
