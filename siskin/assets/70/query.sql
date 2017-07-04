4a5f164acd71e1774e92821b45c3b1db83e699b1
Robert Schenk <schenk@ub.uni-leipzig.de>
2017-07-04 17:15:14 +0200

    update query.sql, refs #5246

--- siskin/assets/70/query.sql
+++ siskin/assets/70/query.sql
@@ -14,42 +14,45 @@ SELECT
     EndPage,
     ISBN,
     Signatur,
-	GROUP_CONCAT(Author , "; " ) As Author,
-	GROUP_CONCAT(Editor , "; " ) As Editor,
-	SeriesTitle.Name as Series
+    GROUP_CONCAT(Author , "; " ) As Author,
+    GROUP_CONCAT(Editor , "; " ) As Editor,
+    SeriesTitle.Name as Series
 FROM
     Reference 
 LEFT JOIN
-	ReferenceAuthor ON (ReferenceAuthor.ReferenceID = Reference.ID)
+    ReferenceAuthor ON (ReferenceAuthor.ReferenceID = Reference.ID)
 LEFT JOIN
-	(SELECT
-		ID as AuthorID,
-		substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Author
-	FROM
-		Person) ON (AuthorID = ReferenceAuthor.PersonID)
+    (SELECT
+        ID as AuthorID,
+        substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Author
+    FROM
+        Person) ON (AuthorID = ReferenceAuthor.PersonID)
 LEFT JOIN
-	ReferenceEditor ON (ReferenceEditor.ReferenceID = Reference.ID)
+    ReferenceEditor ON (ReferenceEditor.ReferenceID = Reference.ID)
 LEFT JOIN
-	(SELECT
-		ID as EditorID,
-		substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Editor
-	FROM
-		Person) ON (EditorID = ReferenceEditor.PersonID)
+    (SELECT
+        ID as EditorID,
+        substr(COALESCE(LastName, '') || ', ' || COALESCE(FirstName, '') || ' ' || COALESCE(MiddleName, ''), 0) as Editor
+    FROM
+        Person) ON (EditorID = ReferenceEditor.PersonID)
 LEFT JOIN
-	ReferencePublisher ON (ReferencePublisher.ReferenceID = Reference.ID)
+    ReferencePublisher ON (ReferencePublisher.ReferenceID = Reference.ID)
 LEFT JOIN
-	Publisher ON (Publisher.ID = ReferencePublisher.PublisherID)
+    Publisher ON (Publisher.ID = ReferencePublisher.PublisherID)
 LEFT JOIN
-	Periodical ON (Periodical.ID = Reference.PeriodicalID)
+    Periodical ON (Periodical.ID = Reference.PeriodicalID)
 LEFT JOIN
-	(SELECT
-		GROUP_CONCAT(CallNumber , "; " ) AS Signatur,
-		ReferenceID
-	FROM
-		Location
-	GROUP BY
-		ReferenceID) as Location2 ON (Location2.ReferenceID = Reference.ID)
+    (SELECT
+        GROUP_CONCAT(CallNumber , "; " ) AS Signatur,
+        ReferenceID
+    FROM
+        Location
+    GROUP BY
+        ReferenceID) as Location2 ON (Location2.ReferenceID = Reference.ID)
 LEFT JOIN
-	SeriesTitle ON (SeriesTitle.ID = Reference.SeriesTitleID)
+    SeriesTitle ON (SeriesTitle.ID = Reference.SeriesTitleID)
+WHERE
+	ReferenceType != "Contribution" AND
+	Title NOT NULL
 GROUP BY
-	Reference.ID
+    Reference.ID
