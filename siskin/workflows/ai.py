fe689b1739896ca7d90ac9a38e8f4c3fcad0a768
Martin Czygan <martin.czygan@gmail.com>
2017-07-28 11:55:30 +0200

    ai: add 34, 101 to groupcover

--- siskin/workflows/ai.py
+++ siskin/workflows/ai.py
@@ -480,7 +480,7 @@ class AIInstitutionChanges(AITask):
         return AILocalData(date=self.date)
 
     def run(self):
-        output = shellout("""groupcover -prefs '85 55 89 60 50 105 49 28 48 121' < {input} > {output}""", input=self.input().path)
+        output = shellout("""groupcover -prefs '85 55 89 60 50 105 34 101 49 28 48 121' < {input} > {output}""", input=self.input().path)
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
