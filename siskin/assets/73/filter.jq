4d0e2554c45f9b1a5e1fd4be7ebc85c9f1196c53
Martin Czygan <martin.czygan@gmail.com>
2017-08-04 15:48:12 +0200

    73: add to siskin, refs #5486, refs #11005

---
+++ siskin/assets/73/filter.jq
@@ -0,0 +1,48 @@
+. |
+{
+	"finc.format":			"ElectronicArticle",
+#	"finc.mega_collection":	"FID #5486",
+	"finc.mega_collection":	"MedienwRezensionen",
+#	"finc.record_id":		["finc-73-",.id]|add,
+	"finc.record_id":		["finc-73-",  # type other or doi
+		(.front."article-meta"."article-id" | map(select(."@pub-id-type" == "other")) | .[]."#text" )] | add,
+    "doi": .front."article-meta"."article-id" | map(select(."@pub-id-type" == "doi")) | .[]."#text" ,
+	"finc.source_id":		"73",
+#	"authors":				[{"rft.au":.creator?}],
+#	"authors":				[{"rft.au":.creator?|add}],
+#	"authors":				.creator?|{reduce .[]} ,
+#	"authors":				[.creator[]|{"rft.au":.}],
+	"authors":	[
+		{"rft.au":
+		.front."article-meta"."contrib-group".contrib | map(select(."@contrib-type" == "author")) | .[].name | [."given-names", " ", ."surname"]|add
+		}
+	],
+#	"url":					[.link],
+	"url":					[.front."article-meta"."self-uri"[]|."@xlink:href"], # or just first [0] # with [] creates multiple records
+    "rft.atitle":			.front."article-meta"."title-group"."article-title",
+#	"rft.date":				([.dateissued,"-01-01"]|add),
+#	"rft.date":				.front."article-meta"."pub-date" | map(select(."@pub-type" == "epub")) | .[] | [.year,"-",.month,"-",.day]|add,
+	"rft.date":				.front."article-meta"."pub-date" | map(select(."@pub-type" == "collection")) | .[] | [.year]|add,
+#    "x.date":           	.front."article-meta"."pub-date" | map(select(."@pub-type" == "epub")) | .[] | [.year,"-",.month,"-",.day , "T00:00:00Z"]|add,
+    "x.date":           	.front."article-meta"."pub-date" | map(select(."@pub-type" == "collection")) | .[] | [.year,"-","01","-","01" , "T00:00:00Z"]|add, # no month/day in oai date/collection
+#	"rft.pub":				[.front."journal-meta".publisher."publisher-name"],
+#	"rft.place":			[.placeofpublication?],
+	"rft.pub":	[.front."journal-meta".publisher."publisher-name" | split(", ")[0]],
+	"rft.place":[.front."journal-meta".publisher."publisher-name" | split(", ")[-1] | gsub("(^[^a-z]+ )|([^a-z]+$)";"";"i") ],
+	"rft.series":			.front."journal-meta"."journal-title",
+#	"rft.jtitle":			.notes, # goes into solr:series, goes into In: # strip prefix:
+#	"rft.jtitle":			(.notes|sub("Erschienen in: ";"")?),
+	"rft.jtitle":	.front."journal-meta"."journal-title",
+	"rft.issue":	.front."article-meta"."issue"."#text",
+	"ris.type":				"ELEC",
+#	"rft.genre":			"unknown",
+#	"x.subjects":			[
+	"issn": [.front."journal-meta".issn[]|."#text"], #both p/e issn,
+	"abstract":				.front."article-meta".abstract.p,
+
+	"version": "0.9"
+}
+
+
+
+# grep "10.17192/ep2011.2.271" extracted/73/73.json | jq .metadata.article -cr | jq -f bin/73/filter.jq
---
+++ siskin/sources/marburg.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+# pylint: disable=F0401,C0111,W0232,E1101,R0904,E1103,C0301
+
+# Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+Medienwissenschaft, Rezensionen, Reviews. Archiv, Marburg, refs #5486, #11005.
+"""
+
+import datetime
+import json
+import sys
+
+import luigi
+import xmltodict
+from gluish.intervals import weekly
+from gluish.parameter import ClosestDateParameter
+from gluish.utils import shellout
+
+from siskin.task import DefaultTask
+
+
+class MarburgTask(DefaultTask):
+    """ Base task for Marburg. """
+    TAG = '73'
+
+    def closest(self):
+        return weekly(self.date)
+
+
+class MarburgCombine(MarburgTask):
+    """ Harvest and combine into a single file. """
+
+    date = ClosestDateParameter(default=datetime.date.today())
+    format = luigi.Parameter(default='nlm')
+
+    def run(self):
+        endpoint = "http://archiv.ub.uni-marburg.de/ep/0002/oai"
+        shellout("metha-sync -format nlm {endpoint}", endpoint=endpoint)
+        output = shellout("metha-cat -format nlm {endpoint} > {output}", endpoint=endpoint)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(self.path())
+
+
+class MarburgJSON(MarburgTask):
+    """
+    Convert XML to JSON in one go. Preparation, so we can use jq(1).
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return MarburgCombine(date=self.date)
+
+    def run(self):
+        with self.input().open() as handle:
+            with self.output().open('w') as output:
+                json.dump(xmltodict.parse(handle.read()), output)
+
+    def output(self):
+        return luigi.LocalTarget(self.path(ext='json'))
+
+
+class MarburgIntermediateSchema(MarburgTask):
+    """
+    Convert to intermediate schema.
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return MarburgJSON(date=self.date)
+
+    def run(self):
+        output = shellout(""" cat {input} | \
+                              jq '.Records.Record[]|.metadata.article' | \
+                              jq -f {filter} -cr > {output}""",
+                          input=self.input().path, filter=self.assets('73/filter.jq'))
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(self.path(ext='ldj'))
