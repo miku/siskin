54857d08ae25a1acd382c31a9dd02fc24560b512
Martin Czygan <martin.czygan@gmail.com>
2017-08-14 09:53:33 +0200

    v0.1.40

--- siskin/__init__.py
+++ siskin/__init__.py
@@ -46,7 +46,7 @@ try:
 except (AttributeError, ImportError):
     pass
 
-__version__ = '0.1.39'
+__version__ = '0.1.40'
 
 config = Config.instance()
 tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
