e42356c1fae93d65e7e251875dbebb07edc4d233
Martin Czygan <martin.czygan@gmail.com>
2017-08-17 15:26:23 +0200

    xmltools: separate handlers, add CountingHandler
    * makes branches a bit shorter
    * CountingHandler instead of grep, customizable tag to use for counting

--- bin/xmltools.py
+++ bin/xmltools.py
@@ -25,20 +25,77 @@
 # @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
 
-
 # Contains a collection of utilities for parsing and analysing xml files
 # Current functions: list_fields, count_records
 # To do: sort tags, count_doublet, print_json
 
+from __future__ import print_function
 
-import collections
 import argparse
-import xml.sax
+import collections
+import io
 import json
 import os
 import re
 import subprocess
+import xml.sax
 
+
+class FieldExampleHandler(xml.sax.ContentHandler):
+    """
+    This handler collects example values for XML tags.
+    """
+
+    def __init__(self):
+        self.examples = {}
+        self.current = (None, None)
+
+    def startElement(self, name, attr):
+        self.current = (name, attr)
+
+    def characters(self, value):
+        name = self.current[0]
+        value = value.strip()
+        if value:
+            if name not in self.examples:
+                self.examples[name] = value
+
+
+class StreamingJsonHandler(xml.sax.ContentHandler):
+    """
+    Read XML and emit JSON in streaming fashion.
+    """
+
+    def __init__(self):
+        self.data = collections.defaultdict(list)
+        self.current = (None, None)
+
+    def startElement(self, name, attr):
+        self.current = (name, attr)
+
+    def endElement(self, name):
+        print(json.dumps(self.data))
+        self.data.clear()
+
+    def characters(self, value):
+        name = self.current[0]
+        value = value.strip()
+        if value:
+            self.data[name].append(value)
+
+
+class CountingHandler(xml.sax.ContentHandler):
+    """
+    This handler counts the number of records (by tag name).
+    """
+
+    def __init__(self, tag='Record'):
+        self.count = 0
+        self.tag = tag
+
+    def endElement(self, name):
+        if name == self.tag:
+            self.count += 1
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v",
@@ -53,74 +110,35 @@ parser.add_argument("-t",
                     dest="task",
                     help="task to do (list_fields, count_records, print_json)",
                     metavar="task")
+parser.add_argument("--tag",
+                    dest="tag",
+                    default="Record",
+                    help="record tag name used for counting",
+                    metavar="name")
+
 args = parser.parse_args()
 
 if args.task == "list_fields":
 
-    class RecordHandler(xml.sax.ContentHandler):
-
-        def __init__(self):
-            self.fieldlist = {}
-            self.current = (None, None)
-
-        def startElement(self, name, attr):
-            self.current = (name, attr)
-
-        def characters(self, value):
-            name = self.current[0]
-            value = value.strip()
-            if value:
-                if name not in self.fieldlist:
-                    self.fieldlist[name] = value
-
     parser = xml.sax.make_parser()
-    handler = RecordHandler()
+    handler = FieldExampleHandler()
     parser.setContentHandler(handler)
     parser.parse(open(args.inputfile, "r"))
 
-    fieldlist = handler.fieldlist
-    for key, value in fieldlist.items():
+    for key, value in handler.examples.items():
         print(key + " = " + value)
 
 elif args.task == "print_json":
 
-    class RecordHandler(xml.sax.ContentHandler):
-
-        def __init__(self):
-            self.data = collections.defaultdict(list)
-            self.current = (None, None)
-
-        def startElement(self, name, attr):
-            self.current = (name, attr)
-
-        def endElement(self, name):
-            print(json.dumps(self.data))
-            self.data.clear()
-
-        def characters(self, value):
-            name = self.current[0]
-            value = value.strip()
-            if value:
-                self.data[name].append(value)
-
     parser = xml.sax.make_parser()
-    handler = RecordHandler()
+    handler = StreamingJsonHandler()
     parser.setContentHandler(handler)
     parser.parse(open(args.inputfile, "r"))
 
 elif args.task == "count_records":
 
-    file = open(args.inputfile, "r", encoding="utf-8")
-    last_line = ""
-    for line in file:
-        forelast_line = last_line
-        last_line = line
-
-    regexp = re.match(".*</(.*)>", forelast_line)
-    if regexp:
-        forelast_tag = regexp.group(1)
-    else:
-        forelast_tag = forelast_line
-
-    command = 'grep -c "</%s>" %s' % (forelast_tag, args.inputfile)
-    os.system(command)
+    parser = xml.sax.make_parser()
+    handler = CountingHandler(tag=args.tag)
+    parser.setContentHandler(handler)
+    parser.parse(open(args.inputfile, "r"))
+    print(handler.count)
