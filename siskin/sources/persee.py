8f8237135552f979f3b7b92da155ece8b44e0a9d
Martin Czygan <martin.czygan@gmail.com>
2017-08-17 12:26:13 +0200

    39: add PerseeMARC task, via asset script

--- siskin/sources/persee.py
+++ siskin/sources/persee.py
@@ -60,3 +60,22 @@ class PerseeCombined(PerseeTask):
 
     def output(self):
         return luigi.LocalTarget(path=self.path(ext='xml.gz'))
+
+
+class PerseeMARC(PerseeTask):
+    """
+    Custom convert to MARC.
+    """
+    date = ClosestDateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return PerseeCombined(date=self.date)
+
+    def run(self):
+        output = shellout("""python {script} <(unpigz -c {input}) {output}""",
+                          script=self.assets('39/39_marcbinary.py'),
+                          input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext='mrc'))
