430723886fdf798bf314506e46177e0f7b367e1a
Martin Czygan <martin.czygan@gmail.com>
2017-08-29 15:54:23 +0200

    ieee: fail earlier

--- siskin/sources/ieee.py
+++ siskin/sources/ieee.py
@@ -126,9 +126,9 @@ class IEEEBacklogIntermediateSchema(IEEETask):
     """
 
     def run(self):
-        output = shellout("""tar xOzf {input} | span-import -i ieee | pigz -c >> {output}""",
+        output = shellout("""tar xOzf {input} | span-import -i ieee | pigz -c > {output}""",
                           input=self.config.get('ieee', 'backlog-archive'),
-                          ignoremap={2: 'FIXME later', 1: 'FIXME soon'})
+                          ignoremap={2: 'FIXME later'})
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
