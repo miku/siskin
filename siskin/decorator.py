3aca558c5414a782b206ec99d890f89e355edbd5
Martin Czygan <martin.czygan@gmail.com>
2017-09-08 10:16:58 +0200

    add deprecated function decorator

---
+++ siskin/decorator.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+# pylint: disable=C0301,C0103
+
+# Copyright 2017 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+import warnings
+import functools
+
+
+def deprecated(func):
+    """
+    Mark functions as deprecated. It will result in a warning being emmitted
+    when the function is used.
+    """
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+	warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+	warnings.warn("call to deprecated function {}.".format(func.__name__),
+		      category=DeprecationWarning, stacklevel=2)
+	warnings.simplefilter('default', DeprecationWarning)  # reset filter
+	return func(*args, **kwargs)
+
+    return new_func
