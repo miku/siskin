1bb22275e2c8fde06e54f082c85c53e8d0b35e14
Robert Schenk <schenk@ub.uni-leipzig.de>
2017-10-23 20:42:36 +0200

    add second collection, refs #8695

--- siskin/assets/148/148_marcbinary.py
+++ siskin/assets/148/148_marcbinary.py
@@ -8,12 +8,13 @@ import sys
 import pymarc
 import marcx
 
-copytags = ("002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "016",
-	        "017", "018", "020", "022", "024", "030", "035", "040", "041", "100", "110", "111", "240", "242", "243",
-	        "245", "246", "247", "249", "250", "260", "263", "300", "310", "362", "490", "500", "501", "502", "504",
-	        "505", "510", "515", "516", "538", "546", "547", "550", "590", "600", "610", "611", "630", "648", "649",
-	        "651", "655", "700", "710", "711", "730", "770", "772", "773", "775", "776", "780", "785", "787",
-	        "800", "810", "811", "830", "856", "906", "982", "999")
+copytags = ("002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014",
+            "016", "017", "018", "020", "022", "024", "030", "035", "040", "041", "100", "110", "111",
+            "240", "242", "243", "245", "246", "247", "249", "250", "260", "263", "300", "310", "362",
+            "490", "500", "501", "502", "504", "505", "510", "515", "516", "538", "546", "547", "550",
+            "590", "600", "610", "611", "630", "648", "649", "651", "655", "700", "710", "711", "730",
+            "770", "772", "773", "775", "776", "780", "785", "787", "800", "810", "811", "830", "856",
+            "906", "982", "999")
 
 # Default input and output.
 inputfilename, outputfilename = "148_input.mrc", "148_output.mrc"
@@ -30,13 +31,13 @@ for oldrecord in reader:
 
     newrecord = marcx.Record(force_utf8=True)
 
-    # prüfen, ob ID und Titel vorhanden sind
-    try:
-    	f001 = oldrecord["001"].data
-    	f245 = oldrecord["245"]["a"]
-    except (KeyError, TypeError):
-    	print("No title found: " + f001)
-    	continue   
+    # prüfen, ob ID vorhanden ist
+    if not oldrecord["001"]:      
+        continue
+
+    # prüfen ob Titel vorhanden ist
+    if not (oldrecord["245"] and oldrecord["245"]["a"]):    
+        continue
 
     # leader
     leader = "     " + oldrecord.leader[5:]
@@ -55,7 +56,7 @@ for oldrecord in reader:
     try:
         f689a = oldrecord["650"]["a"]     
         newrecord.add("689", a=f689a)
-    except (KeyError, TypeError):
+    except (AttributeError, TypeError):
         pass
 
     # 856 (Datensatz) fehlt in der Quelle
@@ -63,7 +64,7 @@ for oldrecord in reader:
 
     # 980
     newrecord.add("980", a=f001, b="148", c="Bundesarchiv (Filmarchiv)")
-
+    newrecord.add("980", a=f001, b="148", c="Verbundkatalog Film")
 
 
     outputfile.write(newrecord.as_marc())
