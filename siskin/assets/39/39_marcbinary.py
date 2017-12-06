0de2cfe959ee84759f948869ebc4288752ecda55
Robert Schenk <schenk@ub.uni-leipzig.de>
2017-12-06 14:02:49 +0100

    change collection name, refs #3133

--- siskin/assets/39/39_marcbinary.py
+++ siskin/assets/39/39_marcbinary.py
@@ -165,7 +165,7 @@ for i, line in enumerate(inputfile, start=1):
     for subject in f950a:
         marcrecord.add("950", a=subject)
 
-    collections = ["a", f001, "b", "39", "c", "Persee", "c", "Persee (adlr)"]
+    collections = ["a", f001, "b", "39", "c", "Persée", "c", "Persée (adlr)"]
     marcrecord.add("980", subfields=collections)
 
     outputfile.write(marcrecord.as_marc())
