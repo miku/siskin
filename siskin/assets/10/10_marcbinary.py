c093087d9250804b015a8975e8b1c4bf2501c7a9
Robert Schenk <schenk@ub.uni-leipzig.de>
2018-02-09 18:44:13 +0100

    10: upload mapping script, refs #5798

---
+++ siskin/assets/10/10_marcbinary.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import io
+import re
+import sys
+import json
+
+import marcx
+
+
+def get_field(tag):
+    try:
+        value = jsonrecord[tag]
+    except:
+        value = ""
+    return value
+
+
+inputfilename = "10_input.json"
+outputfilename = "10_output.mrc"
+
+if len(sys.argv) == 3:
+    inputfilename, outputfilename = sys.argv[1:]
+
+outputfile = io.open(outputfilename, "wb")
+inputfile = open(inputfilename, "r")
+
+for line in inputfile:
+    jsonobject = json.loads(line)
+    jsonrecords = jsonobject["content"]["results"]
+
+    for jsonrecord in jsonrecords:
+
+        marcrecord = marcx.Record(force_utf8=True)
+        marcrecord.strict = False
+
+        # Leader
+        marcrecord.leader = "     ncm  22        4500"
+
+        # Format
+        marcrecord.add("007", data="cr")
+
+        #Identifikator
+        f001 = jsonrecord["id"]
+        f001 = f001.rstrip("/").split("/")[-1]
+        marcrecord.add("001", data="finc-10-" + f001)
+
+        # 1. Urheber
+        f100a = get_field("contributor")
+        if f100a != "":
+            f100a = f100a[0]
+            f100a = f100a.title()
+            marcrecord.add("100", a=f100a)
+
+        # Haupttitel
+        f245a = get_field("title")
+        marcrecord.add("245", a=f245a)
+
+        # Alternativtitel
+        atitles = get_field("other_title")
+        for atitle in atitles:
+            marcrecord.add("246", a=atitle)
+
+        # Erscheinungsjahr
+        f260c = get_field("date")
+        marcrecord.add("260", c=f260c)
+
+        # Schlagwörter
+        subjects = get_field("subject")
+        for subject in subjects:
+            subject = subject.title()
+            marcrecord.add("650", a=subject)
+
+        # weitere Urheber
+        persons = get_field("contributor")
+        if persons != "":
+            for person in persons[1:]:
+                person = person.title()
+                marcrecord.add("700", a=person)
+
+        #n Link zum Datensatz
+        f856u = get_field("url")
+        marcrecord.add("856", q="text/html", _3="Link zur Ressource", u=f856u)
+
+        # Format
+        marcrecord.add("935", b="druck", c="muno")
+
+        # Kollektion
+        marcrecord.add("980", a=f001, b="10", c="Music Treasures Consortium")
+
+        outputfile.write(marcrecord.as_marc())
+
+inputfile.close()
+outputfile.close()
