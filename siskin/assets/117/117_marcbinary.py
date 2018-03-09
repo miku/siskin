12bb60faaaea4351c8b8dc80cadacffea19c1c0f
Robert Schenk <schenk@ub.uni-leipzig.de>
2018-03-09 13:19:22 +0100

    restore old 117_marcbinary.py, refs #8697

--- siskin/assets/117/117_marcbinary.py
+++ siskin/assets/117/117_marcbinary.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 
 from builtins import *
+
 import io
+import re
 import sys
 
-import pymarc
 import marcx
+import pymarc
 from tqdm import tqdm
 
 copytags = ["003", "005", "006", "008", "010", "013", "015", "016",
@@ -21,64 +23,124 @@ copytags = ["003", "005", "006", "008", "010", "013", "015", "016",
             "772", "773", "775", "776", "777", "780", "785", "787", "800", "810", "830",
             "850", "856", "880", "912", "924", "940", "999"]
 
-
-def get_links(record):   
-    for field in record.get_fields("856"):
-        for url in field.get_subfields("u"):
-             yield url
-
-
-def get_titles(record):
-    for field in record.get_fields("772"):
-        for title in field.get_subfields("a"):
-            yield title
-
-def get_field(record, field, subfield="a"):  
-    try:
-        value = record[field][subfield]
-        return value if value is not None else ""
-    except (KeyError, TypeError):
-        return ""
-
 inputfilename, outputfilename = "117_input.mrc", "117_output.mrc"
 
-if len(sys.argv) == 3:
-    inputfilename, outputfilename = sys.argv[1:]
+if len(sys.argv) >= 3:
+    inputfilename, outputfilename = sys.argv[1:3]
+
+# Hidden parameter, total number of records, as a hint for tqdm.
+total = None if len(sys.argv) < 4 else int(sys.argv[3])
 
 inputfile = io.open(inputfilename, "rb")
 outputfile = io.open(outputfilename, "wb")
 
 reader = pymarc.MARCReader(inputfile)
 
-whitelist = set(["AN 1780", "AN 3900", "AN 3920", "AN 4030", "CV 3500", "DW 4000", "DW 4200", "MF 1000", "MF 1500", "NQ 2270"])
+# Filtering by RVK. There are other classification schemes used in 084.2, e.g. in the matched subset:
+#
+#  274772 rvk
+#   34325 sdnb
+#   12453 ssgn
+#    9641 stub
+#    9222 ifzs
+#    1361 sbb
+#     763 dopaed
+#     406 fid
+#     190 z
+#      42 sfb
+#       6 kab
+#       5 ssd
+#       5 ghbs
+#       3 zdbs
 
-for oldrecord in tqdm(reader):
+# A plain whitelist.
+whitelist = set([
+    "AN 1780",
+    "AN 3900",
+    "AN 3920",
+    "AN 4030",
+    "CV 3500",
+    "DW 4000",
+    "DW 4200",
+    "MF 1000",
+    "MF 1500",
+    "NQ 2270"
+])
+
+# Extra patterns.
+pattern_ms = re.compile(r"^MS.7[89][56789].*$")
+pattern_ap = re.compile(r"^AP.*$")
+pattern_f2 = re.compile(r"^AP.99[012].*$")  # via: filter2_relevant_for_FID.xml
+
+# Blacklist of signatures.
+blacklist = set([
+    "AP 6000",
+    "AP 6300",
+    "AP 6400",
+    "AP 6500",
+    "AP 6582",
+    "AP 6583",
+    "AP 6586",
+    "AP 6600",
+    "AP 6630",
+    "AP 6800",
+    "AP 6930",
+    "AP 7200",
+    "AP 7250",
+    "AP 7320",
+    "AP 7337",
+    "AP 7900",
+    "AP 8300",
+    "AP 8735",
+    "AP 8786",
+    "AP 9950",
+    "AP 9954"
+])
+
+
+def filter_084a(value):
+    """
+    A filter helper for a single 084a value. Returns True, if the value passes.
+    """
+    if value in whitelist:
+        return True
+    if pattern_ms.match(value):
+        return True
+    if pattern_ap.match(value) and value not in blacklist and not pattern_f2.match(value):
+        return True
+    return False
+
+
+# Transformation.
+for oldrecord in tqdm(reader, total=total):
 
     newrecord = marcx.Record()
 
+    # via: filter_DE-B170.xml
+    isils = set([s for f in oldrecord.get_fields("049")
+                 for s in f.get_subfields("a")])
+
+    if "DE-B170" not in isils:
+        continue
+
     # prüfen, ob für adlr relevante RVK-Klasse
-    # r = marcx.Record.from_record(oldrecord)
-    # for value in r.itervalues("084.a"): ...
-    rvk = [s for f in oldrecord.get_fields("084") for s in f.get_subfields("a")]
+    rvk = [s for f in oldrecord.get_fields("084")
+           for s in f.get_subfields("a")]
     for value in rvk:
-        if value in whitelist:
-            print("%s whitelisted (%s), ok" % (oldrecord["001"].data, value))
+        if filter_084a(value):
             break
     else:
-        # print("%s not whitelisted, skipping" % oldrecord["001"].data)
         continue
 
     # prüfen, ob Titel vorhanden ist
-    f245 = oldrecord["245"]
-    if not f245:
-        continue   
+    if not oldrecord["245"]:
+        continue
 
     for field in oldrecord.get_fields("856"):
         f856 = field if "http" in field else ""
 
     # leader
-    leader = "     " + oldrecord.leader[5:]
-    newrecord.leader = leader
+    newrecord.leader = "     " + oldrecord.leader[5:]
 
     # 001
     f001 = oldrecord["001"].data
@@ -93,10 +155,11 @@ for oldrecord in tqdm(reader):
     # Originalfelder, die ohne Änderung übernommen werden
     for tag in copytags:
         for field in oldrecord.get_fields(tag):
-            newrecord.add_field(field)    
+            newrecord.add_field(field)
 
     # 980
-    collections = ["a", f001, "b", "117", "c", "UdK Berlin", "c", "Verbundkatalog Film"]
+    collections = ["a", f001, "b", "117", "c",
+                   "UdK Berlin", "c", "Verbundkatalog Film"]
     newrecord.add("980", subfields=collections)
 
     outputfile.write(newrecord.as_marc())
