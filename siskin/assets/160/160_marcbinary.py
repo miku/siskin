7b1a8cbbca5d4c9cb14d51344dc72fb1373cd40c
Robert Schenk <schenk@ub.uni-leipzig.de>
2018-05-03 19:35:07 +0200

    160: upload mapping script, refs #13065

---
+++ siskin/assets/160/160_marcbinary.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+import io
+import sys
+
+import marcx
+import pandas
+import pymarc
+
+inputfilename = "160_input.csv" 
+outputfilename = "160_output.mrc"
+
+if len(sys.argv) == 3:
+    inputfilename, outputfilename = sys.argv[1:]
+
+outputfile = io.open(outputfilename, "wb")
+writer = pymarc.MARCWriter(outputfile)
+
+csv_records = pandas.read_csv(inputfilename, encoding="latin-1", sep=";")
+for csv_record in csv_records.iterrows():
+
+    csv_record = csv_record[1]
+    marc_record = marcx.Record(force_utf8=True)
+    
+    marc_record.leader = "     nam  22        4500"
+
+    f001 = "finc-160-" + str(csv_record["001"])
+    marc_record.add("001", data=f001)
+    
+    marc_record.add("007", data="tu")
+    marc_record.add("041", a=csv_record["041a"])    
+    marc_record.add("100", a=csv_record["100a"])
+    marc_record.add("245", a=csv_record["245a"], c=csv_record["Verfasser\ngesamt"])
+
+    publisher = ["b", "Universität Leipzig, Sportwissenschaftliche Fakultät, ", "c", csv_record["260c"]]
+    marc_record.add("260", subfields=publisher)
+
+    marc_record.add("300", a=csv_record["300a"])
+    marc_record.add("500", a="Signatur: " + csv_record["Signatur\ngesamt"])
+    marc_record.add("502", a=csv_record["502a"])
+    
+    # weitere Urheber
+    for field in csv_record.keys():
+        if not field.startswith("700a"):
+            continue
+        if pandas.isnull(csv_record[field]):
+            continue
+        marc_record.add("700", a=csv_record[field])
+
+    # Medienform
+    marc_record.add("935", b=csv_record["935b"], c=csv_record["935c"])
+
+    # Kollektion
+    marc_record.add("980", a=f001, b="160", c="diplspowi")
+
+
+    writer.write(marc_record)
+    
+writer.close()
