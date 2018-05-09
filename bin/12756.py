c7ca61383c4f51d541b7ebfd00e2c89cc1650193
Martin Czygan <martin.czygan@gmail.com>
2018-05-09 17:32:24 +0200

    add a few checks, refs #12756

--- bin/12756.py
+++ bin/12756.py
@@ -20,9 +20,31 @@ Example:
 
     $ bin/12756.py -s 89 -f author_facet | csvlook -t -H
     ...
+
+    $ bin/12756.py --server http://localhost:8983/solr/biblio --all
+    05/09/2018 17:14:49 [28] [publishDateSort]: 2019 (98)
+    05/09/2018 17:14:50 [30] [publishDateSort]: 1004 (1)
+    05/09/2018 17:14:52 [48] [author_facet]: Copyright 2004 Thomson Media Inc. All Rights Reserved. http: (14888)
+    05/09/2018 17:14:52 [48] [author_facet]: Copyright 2003 Thomson Media Inc. All Rights Reserved. http: (14259)
+    05/09/2018 17:14:52 [48] [author_facet]: Copyright c 2001 Thomson Financial. All Rights Reserved. http: (13903)
+    05/09/2018 17:15:09 [53] [author_facet]: The Polish Institute of International Affairs, Editorial Board (104)
+    05/09/2018 17:15:13 [87] [author_facet]: Chan, Lik Sam; Annenberg School for Communication and Journalism, USC (5)
+    05/09/2018 17:15:13 [87] [author_facet]: Hannah, Mark; University of Southern California, Annenberg School (4)
+    05/09/2018 17:15:13 [87] [author_facet]: Mbunyuza-Memani, Lindani; Southern Illinois University, Carbondale (4)
+    05/09/2018 17:15:13 [87] [author_facet]: Stephens, Niall P.; Department of Communication Arts Framingham State University Framingham Massachusetts, USA (4)
+    05/09/2018 17:15:13 [87] [author_facet]: Anima-Korang, Angela; Southern Illinois University Carbondale (3)
+    05/09/2018 17:15:13 [87] [author_facet]: Faulhaber, Gerald R.; Wharton School, University of Pennsylvania (3)
+    05/09/2018 17:15:13 [87] [author_facet]: Pamment, James; University of Texas at Austin & Karlstad University, Sweden (3)
+    05/09/2018 17:15:13 [87] [author_facet]: Aufderheide, Patricia; Center for Social Media, American University (2)
+    05/09/2018 17:15:13 [87] [author_facet]: Aufderheide, Patricia; School of Communication, American University (2)
+    05/09/2018 17:15:13 [87] [author_facet]: Bai, Yang; College of Communications Pennsylvania State University (2)
+    05/09/2018 17:15:13 [87] [author_facet]: Brantner, Cornelia; Department of Communication, University of Vienna (2)
+    05/09/2018 17:15:16 [105] [publishDateSort]: 1 (1)
+
 """
 
 import argparse
+import datetime
 import json
 import logging
 import sys
@@ -30,16 +52,15 @@ import sys
 import requests
 from six.moves.urllib.parse import urlencode
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S')
-
 
 def facet_response_values(resp):
     """
     Turn a SOLR facet response into a simple sequence of values and counts.
     """
-    ff = resp["facet_counts"]["facet_fields"] 
+    ff = resp["facet_counts"]["facet_fields"]
     for _, v in ff.items():
         for i in range(0, len(v), 2):
             yield v[i], v[i + 1]
@@ -60,6 +81,7 @@ class Solr(object):
             "facet": "on",
             "facet.field": field,
             "facet.mincount": 1,
+            "facet.limit": -1,
             "q": q,
             "rows": 0,
         }
@@ -71,6 +93,20 @@ class Solr(object):
             raise RuntimeError("request failed with %s: %s", r.status_code, link)
         resp = json.loads(r.text)
         return resp
+
+def test_year_is_plausible(year):
+    try:
+        v = int(year)
+        if not 1500 < v < datetime.date.today().year + 1:
+            logging.warn("implaubible year: %s", year)
+    except ValueError:
+        logging.warn("implausible year: %s", year)
+
+def test_field(resp, field, test_func):
+    vcs = [(v, c) for v, c in facet_response_values(resp)]
+    for v, c in vcs:
+        if not test_func(v):
+            logging.warn("[%s] [%s]: %s (%s)", source_id, field, v, c)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("12756")
@@ -101,8 +137,20 @@ if __name__ == '__main__':
             query = "source_id:%s" % (source_id)
             for field in fields:
                 resp = solr.facets(field=field, q=query)
-                for name, count in facet_response_values(resp):
-                    print("%s\t%s" % (name, count))
+
+                # Field specific tests.
+                if field == "publishDateSort":
+                    test_field(resp, field, test_year_is_plausible)
+
+                if field == "author_facet":
+                    test_field(resp, field, lambda v: len(v) < 60)
+
+                if field == "mega_collection":
+                    test_field(resp, field, lambda v: "??" not in v)
+
+                # Just output everything.
+                for value, count in facet_response_values(resp):
+                    pass # print("%s\t%s" % (value, count))
 
         sys.exit(0)
 
@@ -110,4 +158,3 @@ if __name__ == '__main__':
     resp = solr.facets(field=args.field, q=query)
     for name, count in facet_response_values(resp):
         print("%s\t%s" % (name, count))
-
