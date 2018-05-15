090b5a45a39375a84218c9aad09dab5ce406ec12
Robert Schenk <schenk@ub.uni-leipzig.de>
2018-05-15 18:36:45 +0200

    116: fix field order, refs #13120

--- siskin/assets/161/161_marcbinary.py
+++ siskin/assets/161/161_marcbinary.py
@@ -8,11 +8,11 @@ import pymarc
 import marcx
 
 
-copytags = ("003", "005", "006", "007", "008", "040", "043",
-            "050", "082", "100", "245", "246", "250", "264",
-            "300", "336", "337", "338", "490", "500", "505",
-            "520", "600", "610", "650", "651", "700", "710",
-            "776", "830", "856")
+copytags = ("003", "005", "006", "007", "008", "020", "040",
+             "043", "050", "082", "100", "245", "246", "250",
+             "264", "300", "336", "337", "338", "490", "500",
+             "505", "520", "600", "610", "650", "651", "700",
+             "710", "776", "830", "856")
 
 
 inputfilename = "161_input.mrc" 
@@ -43,28 +43,32 @@ for oldrecord in reader:
     f001 = str(f001)
     newrecord.add("001", data="finc-161-" + f001)
 
-    # ISBN
-    try:
-        f020a = oldrecord["020"]["a"]        
-    except:
-        f020a = ""
-
-    if f020a != "":
-        f020a = f020a.replace(" ", "-")
-        f020a = f020a.replace(".", "-")       
-        regexp = re.search("([0-9xX-]{10,17})", f020a)
-     
-        if regexp:
-            f020a = regexp.group(1)
-            f020a = f020a.rstrip("-")
-            newrecord.add("020", a=f020a) 
-        else:
-            print("Die ISBN %s konnte nicht mittels regulärer Ausdrücke überprüft werden." % f020a)
-
-    # Originalfelder, die ohne Änderung übernommen werden
+    # Originalfelder
     for tag in copytags:
-        for field in oldrecord.get_fields(tag):
-            newrecord.add_field(field)   
+
+        # ISBN
+        if tag == "020":
+        
+            try:
+                f020a = oldrecord["020"]["a"]        
+            except:
+                f020a = ""
+
+            if f020a != "":
+                f020a = f020a.replace(" ", "-")
+                f020a = f020a.replace(".", "-")       
+                regexp = re.search("([0-9xX-]{10,17})", f020a)
+             
+                if regexp:
+                    f020a = regexp.group(1)
+                    f020a = f020a.rstrip("-")
+                    newrecord.add("020", a=f020a) 
+                else:
+                    print("Die ISBN %s konnte nicht mittels regulärer Ausdrücke überprüft werden." % f020a)
+
+        else:
+            for field in oldrecord.get_fields(tag):
+                newrecord.add_field(field)   
 
     # Ansigelung
     collections = ["a", f001, "b", "161", "c", "sid-161-col-cambridgeopenaccess"]
