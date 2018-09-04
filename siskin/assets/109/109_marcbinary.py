a2382fb755392946348a9ce610c8e6f099cbffa1
Robert Schenk <schenk@ub.uni-leipzig.de>
2018-09-04 11:33:05 +0200

    109: improve format mapping, refs #8391

--- siskin/assets/109/109_marcbinary.py
+++ siskin/assets/109/109_marcbinary.py
@@ -7,10 +7,87 @@
 
 
 import sys
+import re
 import marcx
 import xmltodict
 import tarfile
-import io
+
+
+formatmap = {
+    "Buch":
+    {
+        "leader": "cam",
+        "007": "tu",
+        "935b": "druck"
+    },
+    "DVD":
+    {
+        "leader": "ngm",
+        "007": "vd",
+        "935b": "dvdv",
+        "935c": "vide"
+    },
+    "Blu-ray":
+    {
+        "leader": "ngm",
+        "007": "vd",
+        "935b": "bray",
+        "935c": "vide"
+    },
+    "Videodatei":
+    {
+        "leader": "cam",
+        "007": "cr",
+        "935b": "cofz",
+        "935c": "vide"
+    },
+    "CD":
+    {
+        "leader": "  m",
+        "007": "c",
+        "935b": "cdda"
+    },
+    "Videokassette":
+    {
+        "leader": "cgm",
+        "007": "vf",
+        "935b": "vika",
+        "935c": "vide"
+    },
+    "Noten":
+    {
+        "leader": "nom",
+        "007": "zm",
+        "935c": "muno"
+    },
+    "Loseblattsammlung":
+    {
+        "leader": "nai",
+        "007": "td",
+    },
+    "Film":
+    {
+        "leader": "cam",
+        "007": "mu",
+        "935b": "sobildtt"
+    },
+    "Aufsatz":
+    {
+        "leader": "naa",
+        "007": "tu"
+    },
+    "Objekt":
+    {
+        "leader": "crm",
+        "007": "zz",
+        "935b": "gegenst"
+    },
+    "Sonstiges":
+    {
+        "leader": "npa",
+        "007": "tu"
+    }
+}
 
 
 def get_datafield(tag, code, all=False):
@@ -35,6 +112,23 @@ def get_datafield(tag, code, all=False):
                 values.append(field["subfield"]["#text"])
     return values
 
+def get_leader(format="Buch"):
+    return "     %s  22        4500" % formatmap[format]["leader"]
+
+def get_field_007(format="Buch"):
+    return formatmap[format]["007"]
+
+def get_field_935b(format="Buch"):
+    if "935b" not in formatmap[format]:
+        return ""
+    return formatmap[format]["935b"]
+
+def get_field_935c(format="Buch"):
+    if "935c" not in formatmap[format]:
+        return ""
+    return formatmap[format]["935c"]
+
+
 inputfilename1 = "109_input1.tar.gz"
 inputfilename2 = "109_input2.tar.gz"
 outputfilename = "109_output.mrc"
@@ -43,7 +137,7 @@ if len(sys.argv) == 4:
     inputfilename1, inputfilename2, outputfilename = sys.argv[1:]
 
 filenames = [inputfilename1, inputfilename2]
-outputfile = io.open(outputfilename, "wb")
+outputfile = open(outputfilename, "wb")
 
 for filename in filenames:
     inputfile = tarfile.open(filename, "r:gz")
@@ -51,7 +145,8 @@ for filename in filenames:
 
     for i, record in enumerate(records):
         if i == 1000:
-            break
+            #break
+            pass
         xmlrecord = inputfile.extractfile(record)
         xmlrecord = xmlrecord.read()
         xmlrecord = xmltodict.parse(xmlrecord)
@@ -60,12 +155,44 @@ for filename in filenames:
         marcrecord = marcx.Record(force_utf8=True)
         marcrecord.strict = False
 
+        # Format
+        format = get_datafield("433", "a")
+        format = str(format)
+        regexp = re.search("S\.\s\d+\s?-\s?\d+", format)
+        if ("S." in format or "Bl." in format or "Ill." in format or " p." in format or "XI" in format or "XV" in format
+                           or "X," in format or "Bde." in format or ": graph" in format):
+            format = "Buch"
+        elif "CD" in format:
+            format = "CD"
+        elif "DVD" in format:
+            format = "DVD"
+        elif "Blu-ray" in format:
+            format = "Blu-ray"
+        elif "Videokassette" in format or "VHS" in format or "Min" in format:
+            format = "Videokassette"
+        elif "Losebl.-Ausg." in format:
+            format = "Loseblattsammlung"
+        elif regexp:
+            format = "Aufsatz"
+        elif ("Plakat" in format or "Kassette" in format or "Box" in format or "Karton" in format or "Postkarten" in format
+                                 or "Teile" in format or "USB" in format or "Schachtel" in format or "Schautafel" in format
+                                 or "Medienkombination" in format or "Tafel" in format or "Faltbl" in format or "Schuber" in format):
+            format = "Objekt"
+        else:
+            print("unbekannt:", format, "Default = Sonstiges")
+            format = "Sonstiges"
+
         # Leader
-        marcrecord.leader = "     nab  22        4500"
+        leader = get_leader(format=format)
+        marcrecord.leader = leader
 
         # Identifier
         f001 = get_datafield("001", "a")
         marcrecord.add("001", data="finc-109-" + f001)
+
+        # 007
+        f007 = get_field_007(format=format)
+        marcrecord.add("007", data=f007)
 
         # ISBN
         f020a = get_datafield("540", "a")
@@ -118,9 +245,14 @@ for filename in filenames:
         for tag in range(200, 300):
             f710a = get_datafield(str(tag), "a")
             marcrecord.add("710", a=f710a)
+
+        # Format
+        f935b = get_field_935b(format=format)
+        f935c = get_field_935c(format=format)
+        marcrecord.add("935", b=f935b, c=f935c)
                     
         # Kollektion
-        collection = ["a", f001, "b", "109", "c", u"Kunsthochschule für Medien Köln", "c", "Verbundkatalog Film"]
+        collection = ["a", f001, "b", "109", "c", "Kunsthochschule für Medien Köln", "c", "Verbundkatalog Film"]
         marcrecord.add("980", subfields=collection)
 
         outputfile.write(marcrecord.as_marc())
