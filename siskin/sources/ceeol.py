9ca9752fd4fafb0ff0d0a69daea390da6bbf7b02
Martin Czygan <martin.czygan@gmail.com>
2018-11-07 17:04:58 +0100

    ceeol: dump plus updates

--- siskin/sources/ceeol.py
+++ siskin/sources/ceeol.py
@@ -25,6 +25,9 @@
 """
 https://www.ceeol.com/, refs #9398.
 
+Currently, there are two XML formats used, a custom CEEOL and a slightly
+invalid MARCXML. The last update was manually uploaded to a Server @Telekom.
+
 ----
 
 Config:
@@ -32,7 +35,7 @@ Config:
 [ceeol]
 
 disk-dir = /path/to/disk
-
+updates = /path/to/file.xml,/path/to/another.xml,...
 
 """
 
@@ -40,6 +43,7 @@ from __future__ import print_function
 
 import glob
 import os
+import hashlib
 import tempfile
 
 import luigi
@@ -54,18 +58,43 @@ class CeeolTask(DefaultTask):
     """
     TAG = '53'
 
+class CeeolJournalsUpdates(CeeolTask):
+    """
+    Create an intermediate schema from zero or more update XML (in MARCXML).
+    The output will depend on the input files.
+    """
+    def run(self):
+        paths = [p.strip() for p in self.config.get("ceeol", "updates").split(",")]
+        _, stopover = tempfile.mkstemp(prefix="siskin-")
+        for p in paths:
+            shellout("span-import -i ceeol-marcxml {input} >> {output}", input=p, output=stopover)
+        luigi.LocalTarget(stopover).move(self.output().path)
 
-class CeeolJournalsDumpIntermediateSchema(CeeolTask):
+    def output(self):
+        """
+        Only use the string of files as hashed identifier. XXX: It would be
+        more robust to use the checksums of all input files
+        """
+        sha1 = hashlib.sha1()
+        sha1.update(self.config.get("ceeol", "updates"))
+        output = os.path.join(self.taskdir(), sha1.hexdigest()) + ".ndj"
+        return luigi.LocalTarget(path=output)
+
+class CeeolJournalsIntermediateSchema(CeeolTask):
     """
-    Combine all journals from disk dump and convert them to intermediate schema.
+    Combine all journals from disk dump and convert them to intermediate schema. Add updates.
     """
+    def requires(self):
+        return CeeolJournalsUpdates()
 
     def run(self):
         diskdir = self.config.get('ceeol', 'disk-dir')
         _, stopover = tempfile.mkstemp(prefix='siskin-')
         for path in glob.glob(os.path.join(diskdir, '1Journal-Articles-XML', 'articles_*xml')):
             shellout("span-import -i ceeol {input} | pigz -c >> {output}", input=path, output=stopover)
+        shellout("cat {update} | pigz -c >> {output}", update=self.input().path, output=stopover)
         luigi.LocalTarget(stopover).move(self.output().path)
 
     def output(self):
         return luigi.LocalTarget(path=self.path(ext="ldj.gz"))
+
