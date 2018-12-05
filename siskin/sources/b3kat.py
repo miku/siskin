df3a5365808454a1d7e456e7c92e7916fd72bec7
Martin Czygan <martin.czygan@gmail.com>
2018-12-05 14:30:27 +0100

    b3kat: update docs

--- siskin/sources/b3kat.py
+++ siskin/sources/b3kat.py
@@ -26,6 +26,46 @@
 
 """
 B3Kat, #8697.
+
+The Bavarian Library Network (https://www.bib-bvb.de/) hosts B3Kat (around 228 institutions in 2018).
+
+> Der B3Kat ist die gemeinsame Katalogisierungsplattform der
+Bibliotheksverbünde BVB und KOBV. Die Hochschulbibliotheken der drei
+Bundesländer Bayern, Berlin und Brandenburg, die Bayerische Staatsbibliothek
+sowie zahlreiche Spezialbibliotheken rücken mit dem Aufbau des B3Kat näher
+zusammen. Der B3Kat bildet das Kernstück der Strategischen Allianz des BVB
+zusammen mit dem KOBV.
+
+```
+>>> import pandas as pd
+>>> pd.read_html("http://www.gateway-bayern.de/bvb_biblist_sort_ger.html")[0].head()
+Typ    ISIL Sigel Kürzel                                    Bibliotheksname         Ort Recherche
+0    1   DE-12    12  BSB01               Bayerische  Staatsbibliothek München     München   Katalog
+1    2  DE-384   384  UBA01                    Universitätsbibliothek Augsburg    Augsburg   Katalog
+2    2  DE-473   473  UBG01                    Universitätsbibliothek  Bamberg     Bamberg   Katalog
+3    2  DE-703   703  UBT01                   Universitätsbibliothek  Bayreuth    Bayreuth   Katalog
+4    2  DE-945   945  UEI03  Universitätsbibliothek Eichstätt -  Wirtschaft...  Ingolstadt   Katalog
+
+>>> df.groupby('Ort').size().sort_values(ascending=False).head(15)
+Ort
+München       69
+Regensburg    19
+Berlin        19
+Augsburg      15
+Bamberg        7
+Würzburg       6
+Ingolstadt     6
+Nürnberg       6
+Hof            4
+Freising       4
+Passau         3
+Erlangen       3
+Coburg         3
+Fürth          3
+Ansbach        3
+dtype: int64
+```
+
 """
 
 import datetime
@@ -69,8 +109,8 @@ class B3KatLinks(B3KatTask):
 
 class B3KatDownload(B3KatTask):
     """
-    Download snapshot. Output is a single (large) MARC binary file. Strip
-    non-sorting characters (U+0098, U+009C) from raw data. XXX: Single core.
+    Download snapshot. Output is a single (large) MARC binary file. Typically
+    the downloads are provided in May and November.
     """
     date = ClosestDateParameter(default=datetime.date.today())
 
@@ -78,18 +118,12 @@ class B3KatDownload(B3KatTask):
         return B3KatLinks(date=self.date)
 
     def run(self):
-        """
-        XXX: wrapped sed ineffective.
-        """
         _, stopover = tempfile.mkstemp(prefix='siskin-')
         with self.input().open() as handle:
             for i, row in enumerate(handle.iter_tsv(cols=('url',)), start=1):
                 downloaded = shellout("""curl -sL --fail "{url}" > {output} """, url=row.url)
-                cleaned = shellout(r"""CHARS=$(python -c 'print u"\u0098\u009C".encode("utf8")');
-                                       unpigz -c {input} | sed 's/['"$CHARS"']//g' > {output} """, input=downloaded)
-                output = shellout("""yaz-marcdump -i marcxml -o marc "{input}" > {output}""", input=cleaned)
-                shellout("cat {input} >> {stopover}",
-                         input=output, stopover=stopover)
+                output = shellout("""yaz-marcdump -i marcxml -o marc "{input}" >> {stopover}""",
+                                  input=downloaded, stopover=stopover)
                 try:
                     os.remove(downloaded)
                     os.remove(cleaned)
