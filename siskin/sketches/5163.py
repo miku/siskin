7567154df9c3b8a996ef5311edef11c89f8b1e18
Martin Czygan <martin.czygan@gmail.com>
2018-12-18 20:32:49 +0100

    68: add intermediate stats on doi validity

--- siskin/sketches/5163.py
+++ siskin/sketches/5163.py
@@ -4,10 +4,19 @@
 """
 Refs 5163. Download link at #note-65. Wget is your friends. 250M zipped, 860000 records.
 
+Usage:
+
+    $ python 5163.py > results.json
+    $ jq -rc .dois[] results.json | awk '{ print "http://doi.org/"$0 }' | clinker -w 200 > ping.json
+    $ jq .status ping.json | sort | uniq -c | sort -nr
+    3900 200
+    132  404
+    11   403
+
 Problems:
 
 * Already in SOLR format. Attachments (78/2.) needs IS (for now).
-* Only 5% have DOI for deduplication.
+* Only about 5% have DOI for deduplication?
 
 ## How many ISSN?
 
@@ -23,52 +32,54 @@ import collections
 import gzip
 import re
 
+import requests
 import six
 import tqdm
 
 import ujson as json
 
-pattern = re.compile(r'[^0-9/a-z]10[.][0-9a-zA-Z]*/[0-9a-zA-Z]{4,}')
+pattern = re.compile(r'10[.][0-9a-zA-Z]*/[0-9a-zA-Z]{4,}')
 
 if __name__ == '__main__':
 
-    counter = collections.Counter()
+    # counter = collections.Counter()
     dois = collections.Counter()
 
     with gzip.open("dswarm-68-20181008150514_all.ldj.gz", "rb") as handle:
-        for line in handle:
+        for line in tqdm.tqdm(handle):
             matches = pattern.findall(line)
-            doc = json.loads(line)
-
             for match in matches:
-                match = match.strip().strip('"')
-                dois[match] += 1
+                candidate = match.strip().strip('"')
+                dois[candidate] += 1
 
-            output = {
-                "finc.format": doc.get("format", "ElectronicArticle"),
-                "finc.id": doc["id"],
-                "finc.mega_collection": doc.get("mega_collection", []),
-                "finc.record_id": doc["record_id"],
-                "finc.source_id": doc["source_id"],
-                "languages": doc.get("language", "eng"),
-                "rft.atitle": doc.get("title"),
-                "rft.issn": doc.get("issn", []),
-                "rft.pub": doc.get("publisher"),
-                "rft.place": doc.get("place"),
-                "rft.date": '%s-01-01' % doc.get("publishDateSort", "1970"),
-                "subjects": doc.get("topic"),
-                "version": "0.9",
-                "rft.authors": [{"rft.aucorp": v} for v in doc.get("author_corporate2", [])],
-            }
+            # doc = json.loads(line)
+            # output = {
+            #     "finc.format": doc.get("format", "ElectronicArticle"),
+            #     "finc.id": doc["id"],
+            #     "finc.mega_collection": doc.get("mega_collection", []),
+            #     "finc.record_id": doc["record_id"],
+            #     "finc.source_id": doc["source_id"],
+            #     "languages": doc.get("language", "eng"),
+            #     "rft.atitle": doc.get("title"),
+            #     "rft.issn": doc.get("issn", []),
+            #     "rft.pub": doc.get("publisher"),
+            #     "rft.place": doc.get("place"),
+            #     "rft.date": '%s-01-01' % doc.get("publishDateSort", "1970"),
+            #     "subjects": doc.get("topic"),
+            #     "version": "0.9",
+            #     "rft.authors": [{"rft.aucorp": v} for v in doc.get("author_corporate2", [])],
+            # }
 
             # Frequency of ISSN.
-            for issn in output["rft.issn"]:
-                counter[issn] += 1
+            # for issn in output["rft.issn"]:
+            #     counter[issn] += 1
 
             # print(json.dumps(output))
 
-    print(len(counter))
-    print(len(dois))
+    # print(len(counter))
+    # print(len(dois))
 
-    for doi, count in dois.most_common(n=10):
-        print(count, doi)
+    print(json.dumps({
+        "dois": [doi for doi, _ in dois.most_common()],
+        "doi_count": len(dois),
+    }))
