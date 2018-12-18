0646363e06df8301cf86c55a204c04f0708e3736
Martin Czygan <martin.czygan@gmail.com>
2018-12-18 18:57:18 +0100

    68: collect more information, refs #5163

--- siskin/sketches/5163.py
+++ siskin/sketches/5163.py
@@ -9,16 +9,41 @@ Problems:
 * Already in SOLR format. Attachments (78/2.) needs IS (for now).
 * Only 5% have DOI for deduplication.
 
+## How many ISSN?
+
+About 638 unique ISSN.
+
+## How many DOI?
+
+XXX.
+
 """
 
+import collections
 import gzip
-import ujson as json
+import re
+
+import six
 import tqdm
 
+import ujson as json
+
+pattern = re.compile(r'[^0-9/a-z]10[.][0-9a-zA-Z]*/[0-9a-zA-Z]{4,}')
+
 if __name__ == '__main__':
+
+    counter = collections.Counter()
+    dois = collections.Counter()
+
     with gzip.open("dswarm-68-20181008150514_all.ldj.gz", "rb") as handle:
-        for line in tqdm.tqdm(handle):
+        for line in handle:
+            matches = pattern.findall(line)
             doc = json.loads(line)
+
+            for match in matches:
+                match = match.strip().strip('"')
+                dois[match] += 1
+
             output = {
                 "finc.format": doc.get("format", "ElectronicArticle"),
                 "finc.id": doc["id"],
@@ -35,5 +60,15 @@ if __name__ == '__main__':
                 "version": "0.9",
                 "rft.authors": [{"rft.aucorp": v} for v in doc.get("author_corporate2", [])],
             }
-            print(json.dumps(output))
 
+            # Frequency of ISSN.
+            for issn in output["rft.issn"]:
+                counter[issn] += 1
+
+            # print(json.dumps(output))
+
+    print(len(counter))
+    print(len(dois))
+
+    for doi, count in dois.most_common(n=10):
+        print(count, doi)
