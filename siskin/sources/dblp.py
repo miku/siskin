6278d6fcdac24f8ea08c6ff7550fcf52699d346b
Martin Czygan <martin.czygan@gmail.com>
2018-12-21 19:16:20 +0100

    dblp: fix link

--- siskin/sources/dblp.py
+++ siskin/sources/dblp.py
@@ -72,8 +72,7 @@ class DBLPDOIList(DBLPTask):
 
     def run(self):
         output = shellout("""LC_ALL=C grep "doi.org" <(unpigz -c {input}) |
-                             LC_ALL=C sed -e 's@<ee>http://dx.doi.org/@@g' |
-                             LC_ALL=C sed -e 's@</ee>@@g' |
+                             LC_ALL=C sed -e 's@<ee>https://doi.org/@@g;s@</ee>@@g' |
                              LC_ALL=C grep ^10 |
                              LC_ALL=C sort -S50% > {output}""", input=self.input().path)
         luigi.LocalTarget(output).move(self.output().path)
