98879f4363fb4ca6af7c84189de8d1e08d8e4508
Martin Czygan <martin.czygan@gmail.com>
2019-01-08 16:33:03 +0100

    172: adjust indentation
    > Thus spake the Lord: Thou shalt indent with four spaces.
    
    But also:
    
    > There are plenty of very good reasons to use tabs; the question is:
    are there any really good reasons not to? And as far as I can tell the
    only one is "whoever wrote PEP-8 didn't like them" which seems a bit
    weak.
    
    Via: https://stackoverflow.com/q/119562/89391

--- siskin/assets/172/172_marcbinary.py
+++ siskin/assets/172/172_marcbinary.py
@@ -14,7 +14,7 @@ inputfilename = "172_input.tsv"
 outputfilename = "172_output.mrc"
 
 if len(sys.argv) == 3:
-	inputfilename, outputfilename = sys.argv[1:]
+    inputfilename, outputfilename = sys.argv[1:]
 
 inputfile = open(inputfilename, "r")
 outputfile = open(outputfilename, "wb")
@@ -22,75 +22,74 @@ outputfile = open(outputfilename, "wb")
 records = inputfile.readlines()
 
 for line in records[1:]:
-	
-	fields = line.split("	")
 
-	marcrecord = marcx.Record(force_utf8=True)
-	marcrecord.strict = False
-	
-	# Leader
-	marcrecord.leader = "     naa  22        4500"
+    fields = line.split("   ")
 
-	# ID
-	f001 = fields[0]
-	marcrecord.add("001", data="finc-172-" + f001)
+    marcrecord = marcx.Record(force_utf8=True)
+    marcrecord.strict = False
 
-	# 007
-	marcrecord.add("007", data="cr")
+    # Leader
+    marcrecord.leader = "     naa  22        4500"
 
-	# Sprache
-	f041a = fields[10]
-	marcrecord.add("041", a=f041a)
+    # ID
+    f001 = fields[0]
+    marcrecord.add("001", data="finc-172-" + f001)
 
-	# 1. Schöpfer
-	persons = fields[4].split("; ")
-	f100a = persons[0]
-	marcrecord.add("100", a=f100a)
-	
-	# Haupttitel	
-	f245a = fields[6]	
-	marcrecord.add("245", a=f245a)
+    # 007
+    marcrecord.add("007", data="cr")
 
-	# Erscheinungsjahr
-	f260c = fields[13]
-	if len(f260c) == 5:	
-		marcrecord.add("260", c=f260c)
+    # Sprache
+    f041a = fields[10]
+    marcrecord.add("041", a=f041a)
 
-	# Reihe
-	f490a = fields[2]	
-	marcrecord.add("490", a=f490a)
+    # 1. Schöpfer
+    persons = fields[4].split("; ")
+    f100a = persons[0]
+    marcrecord.add("100", a=f100a)
 
-	# Rechtehinweis
-	f500a = fields[3]	
-	marcrecord.add("500", a=f500a)
+    # Haupttitel
+    f245a = fields[6]
+    marcrecord.add("245", a=f245a)
 
-	# Dateiformat
-	f500a = fields[9]	
-	marcrecord.add("500", a="Dateiformat: " + f500a)
+    # Erscheinungsjahr
+    f260c = fields[13]
+    if len(f260c) == 5:
+        marcrecord.add("260", c=f260c)
 
-	# Beschreibung
-	f520a = fields[1]	
-	marcrecord.add("520", a=f520a)
+    # Reihe
+    f490a = fields[2]
+    marcrecord.add("490", a=f490a)
 
-	# Schlagwörter
-	f650a = fields[11]	
-	marcrecord.add("650", a=f650a)
+    # Rechtehinweis
+    f500a = fields[3]
+    marcrecord.add("500", a=f500a)
 
-	# weitere Schöpfer
-	for person in persons[1:]:
-		marcrecord.add("700", a=person)
-	
-	# Link zur Ressource
-	f856u = fields[12]	
-	marcrecord.add("856", q="text/html", _3="Link zur Ressource", u=f856u)
+    # Dateiformat
+    f500a = fields[9]
+    marcrecord.add("500", a="Dateiformat: " + f500a)
 
-	# Medienform
-	marcrecord.add("935", b="cofz")
+    # Beschreibung
+    f520a = fields[1]
+    marcrecord.add("520", a=f520a)
 
-	# Kollektion
-	marcrecord.add("980", a=f001, b="172", c="sid-172-col-opal")
+    # Schlagwörter
+    f650a = fields[11]
+    marcrecord.add("650", a=f650a)
 
+    # weitere Schöpfer
+    for person in persons[1:]:
+        marcrecord.add("700", a=person)
 
-	outputfile.write(marcrecord.as_marc())
+    # Link zur Ressource
+    f856u = fields[12]
+    marcrecord.add("856", q="text/html", _3="Link zur Ressource", u=f856u)
+
+    # Medienform
+    marcrecord.add("935", b="cofz")
+
+    # Kollektion
+    marcrecord.add("980", a=f001, b="172", c="sid-172-col-opal")
+
+    outputfile.write(marcrecord.as_marc())
 
 outputfile.close()
