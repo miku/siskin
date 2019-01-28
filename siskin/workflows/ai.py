be7131c27f018a89efaf5e252b1afc0b6243446d
Martin Czygan <martin.czygan@gmail.com>
2019-01-28 19:39:14 +0100

    ai: use doaj format

--- siskin/workflows/ai.py
+++ siskin/workflows/ai.py
@@ -279,7 +279,7 @@ class AIIntermediateSchema(AITask):
             ArxivIntermediateSchema(date=self.date, stamp=True),
             CrossrefIntermediateSchema(date=self.date, stamp=True),
             DegruyterIntermediateSchema(date=self.date, stamp=True),
-            DOAJIntermediateSchema(date=self.date, stamp=True, format="doaj-api"),
+            DOAJIntermediateSchema(date=self.date, stamp=True, format="doaj"),
             ElsevierJournalsIntermediateSchema(date=self.date, stamp=True),
             GenderopenIntermediateSchema(date=self.date, stamp=True),
             GeniosCombinedIntermediateSchema(date=self.date, stamp=True),
