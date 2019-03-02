369167221a2fc5d271326553802ed8f56a307222
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-03-02 10:18:05 +0100

    39: adapt mapping script to new input format (marcxml)
    refs #11349

--- siskin/assets/39/39_marcbinary.py
+++ siskin/assets/39/39_marcbinary.py
@@ -1,188 +1,64 @@
-#!/usr/bin/dev python3
+#!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
+from builtins import *
 
-import base64
 import io
 import re
 import sys
 
 import marcx
+import pymarc
 
 
-def get_field(field):
-
-    if field.find("</") != -1:
-        pattern = field
-    else:
-        pattern = "<%s.*?>(.*?)<\/%s>" % (field, field)
-
-    regexp = re.search(pattern, line)
-    if regexp:
-        value = regexp.group(1)
-        return value
-    else:
-        return ""
+copytags = ["001", "003", "005", "007", "008", "022", "024", "040", "041", "100", "102",
+            "108", "110", "112", "115", "120", "123", "124", "130", "137", "139", "140",
+            "144", "146", "150", "151", "152", "159", "160", "166", "174", "175", "177",
+            "178", "179", "180", "181", "182", "183", "184", "185", "186", "187", "189",
+            "190", "191", "192", "193", "194", "195", "196", "197", "198", "199", "200",
+            "206", "211", "236", "245", "256", "260", "326", "363", "370", "377", "391",
+            "411", "501", "506", "520", "542", "550", "600", "604", "650", "653", "690",
+            "700", "710", "742", "760", "773", "777", "787", "797", "856", "860", "873",
+            "880", "947", "948", "961"]
 
 
-def get_repeatable_field(field):
+inputfilename = "39_input.xml"
+outputfilename = "39_output.mrc"
 
-    if field.find("</") != -1:
-        pattern = field
-    else:
-        pattern = "(<%s.*?>.*<\/%s>)" % (field, field)
+if len(sys.argv) >= 3:
+    inputfilename, outputfilename = sys.argv[1:3]
 
-    regexp = re.search(pattern, line)
-    if regexp:
-        value = regexp.group(1)
-        value = value.replace("<%s>" % field, "")
-        value = value.split("</%s>" % field)
-    else:
-        value = []
-    return value
+inputfile = io.open(inputfilename, "rb")
+outputfile = io.open(outputfilename, "wb")
 
-input, output = "39_input.xml", "39_output.mrc"
+reader = pymarc.parse_xml_to_array(inputfile)
 
-if len(sys.argv) == 3:
-    input, output = sys.argv[1], sys.argv[2]
+for oldrecord in reader:
 
-inputfile = io.open(input, "r", encoding="utf-8")
-outputfile = io.open(output, "wb")
-
-for i, line in enumerate(inputfile, start=1):
-
-    line = line.replace("\n", "")
-
-    # Zugangsrechte
-    if "<dcterms:accessRights>restricted</dcterms:accessRights>" in line:
+    newrecord = marcx.Record()
+    newrecord.strict = False
+    
+    # prüfen, ob Titel vorhanden ist
+    if not oldrecord["245"]:
         continue
 
-    # Identfier
-    f001 = get_field("identifier")
-    if f001 == "":
-        continue
-    f001 = f001
-    f001 = f001.encode("utf-8")
-    f001 = base64.b64encode(f001)
-    f001 = f001.decode("ascii")
-    f001 = f001.rstrip("=")
+    # leader
+    newrecord.leader = "     " + oldrecord.leader[5:]
 
-    # ISSN
-    f022a = get_field("<dc:identifier scheme=\"ISSN\">issn:(.*)</dc:identifier>")
+    # 001   
+    f001 = oldrecord["001"].data
+    newrecord.add("001", data="finc-39-%s" % f001)
+    
+    # Originalfelder, die ohne Änderung übernommen werden
+    for tag in copytags:
+        for field in oldrecord.get_fields(tag):
+            newrecord.add_field(field)      
 
-    # Sprache
-    f041a = get_field("<dc:language>(.*)</dc:language>")
-
-    # Person
-    persons = get_repeatable_field("dc:creator")
-
-    # Titel
-    f245a = get_field("dc:title")
-    try:
-        f245a, f245b = f245a.split(" : ")
-    except:
-        f245b = ""
-
-    # Erscheinungsvermerk
-    f260b = get_field("dc:publisher")
-    try:
-        f260a, f260b = f260b.split(" : ")
-    except:
-        f260a = ""
-    f260c = get_field("dc:date")
-
-    # Umfang
-    f300a = get_field("dcterms:extent")
-    f300a = f300a
-    if f300a != "":
-        regexp = re.search("(\d+)\D", f300a)
-        if regexp:
-            f300a = regexp.group(1)
-    else:
-        f300a = ""
-
-    # ausführliche Quellenangabe
-    f500a = get_field("dcterms:bibliographicCitation")
-
-    # beschreibender Text
-    f520a = get_field("<dc:description xml:lang=\"\w\w\w\">(.*?)</dc:description>")
-
-    FIELD_LENGTH_LIMIT = 8000  # Limit is 9999 bytes, but as we might not only have ASCII, add a margin.
-
-    if len(f520a) > FIELD_LENGTH_LIMIT:
-        print("cutting off long description (%s -> %s) for %s" % (len(f520a), FIELD_LENGTH_LIMIT, get_field("identifier")), file=sys.stderr)
-        f520a = f520a[:FIELD_LENGTH_LIMIT]  # Cut off long abstracts, refs #11349.
-
-    format = get_field("dc:type")
-    if format == "notebiblio":
-        leader = "     naa  22        4500"
-    elif format == "illustration":
-        leader = "     ckm  22        4500"
-    elif format == "liminaire":
-        leader = "     naa  22        4500"
-    elif format == "table":
-        leader = "     naa  22        4500" 
-    elif format == "book":
-        leader = "     cam  22        4500"   
-    else:
-        leader = "     cab  22        4500"
-
-    # Quelle
-    f773a = get_field("dcterms:bibliographicCitation\.jtitle")
-    volume = get_field("dcterms:bibliographicCitation\.volume")
-    issue = get_field("dcterms:bibliographicCitation\.issue")
-    spage = get_field("dcterms:bibliographicCitation\.spage")
-    epage = get_field("dcterms:bibliographicCitation\.epage")
-    f773g = "%s(%s)%s, S. %s-%s" % (volume, f260c, issue, spage, epage)
-    f773x = get_field("<dc:identifier scheme=\"ISSN\">issn:(.*)</dc:identifier>")
-
-    # URL und DOI
-    f856u = get_field("dc:identifier")
-    doi = get_field("<dc:identifier scheme=\"DOI\">(.*?)</dc:identifier>")
-
-    # Schlagwörter
-    f950a = get_field("<dc:subject xml:lang=\"\w\w\w\">(.*?)</dc:subject>")
-    f950a = f950a.split(" ; ")
-
-    marcrecord = marcx.Record(force_utf8=True, to_unicode=True)
-    marcrecord.strict = False
-    marcrecord.leader = leader
-    marcrecord.add("001", data="finc-39-%s" % f001)
-    marcrecord.add("007", data="cr")
-    #marcrecord.add("008", data=f008)
-    marcrecord.add("022", a=f022a)
-    marcrecord.add("041", a=f041a)
-
-    if len(persons) > 0:
-        f100a = persons[0]
-    else:
-        f100a = ""
-
-    marcrecord.add("100", a=f100a)
-    marcrecord.add("245", subfields=["a", f245a, "b", f245b])
-    marcrecord.add("260", a=f260a, b=f260b, c=f260c)
-    marcrecord.add("300", a=f300a)
-    marcrecord.add("500", a=f500a)
-    marcrecord.add("520", a=f520a)
-
-    if len(persons) > 1:
-        for f700a in persons[1:]:
-            marcrecord.add("700", a=f700a)
-
-    marcrecord.add("773", a=f773a, g=f773g, x=f773x)
-    marcrecord.add("856", q="text/html", _3="Link zur Ressource", u=f856u)
-
-    if doi != "":
-        marcrecord.add("856", q="text/html", _3="DOI", u="http://doi.org/" + doi)
-
-    for subject in f950a:
-        marcrecord.add("950", a=subject)
-
-    collections = ["a", f001, "b", "39", "c", "sid-39-col-persee", "c", "sid-39-col-perseeadlr"]
-    marcrecord.add("980", subfields=collections)
-
-    outputfile.write(marcrecord.as_marc())
+    # 980
+    collections = ["a", f001, "b", "39", "c", u"sid-39-col-persee", "c", u"sid-39-col-perseeadlr"]
+    newrecord.add("980", subfields=collections)
+ 
+    outputfile.write(newrecord.as_marc())
 
 inputfile.close()
 outputfile.close()
