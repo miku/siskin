1340c407cdb3ba1a6f16695473bffb39a89fb413
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-06-27 14:06:24 +0200

    35: add mapping script, refs #15487

---
+++ siskin/assets/35/35_marcbinary.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# SID: 35
+# Ticket: #14511, #15487
+# TechnicalCollectionID: sid-35-col-hathi
+# Task: TODO
+
+
+from __future__ import print_function
+
+import re
+import sys
+
+import pymarc
+
+import marcx
+from io import StringIO, BytesIO
+from siskin.mappings import formats
+
+
+setlist = ["hathitrust:pd"]
+
+inputfilename = "35_input.xml"
+outputfilename = "35_output.mrc"
+
+if len(sys.argv) == 3:
+    inputfilename, outputfilename = sys.argv[1:]
+
+inputfile = open(inputfilename, "r", encoding='utf-8')
+outputfile = open(outputfilename, "wb")
+
+pattern = re.compile(r"<setSpec>(.*?)</setSpec>.*<metadata>(.*)</metadata>")
+
+for line in (line.strip() for line in inputfile if line.strip()):
+
+    result = pattern.search(line)
+    if not result:
+        continue
+
+    set, marc = result.groups()
+
+    if set not in setlist:
+        continue
+
+    sio = StringIO(marc)
+    parsed = pymarc.marcxml.parse_xml_to_array(sio)
+    if len(parsed) != 1:
+        raise ValueError("Der Record entspricht nicht genau einer Zeile: " + parsed)
+    marcrecord = marcx.Record.from_record(parsed[0])
+    marcrecord.strict = False
+
+    # Leader
+    marcrecord.leader = "     " + marcrecord.leader[5:]
+
+    # Identifikator   
+    f001 = marcrecord["856"]["u"]
+    match = re.search(".*\/(.*)", f001)
+    if match:
+        f001 = match.group(1)
+        f001 = f001.replace(".", "")
+        marcrecord.remove_fields("001")
+        marcrecord.add("001", data="finc-35-%s" % f001)
+    else:
+        continue
+
+    # Zugangsfacette
+    marcrecord.add("007", data="cr")
+
+    # Profilierung
+    try:
+        f050a = marcrecord["050"]["a"]
+    except:
+        continue
+    
+    if f050a:
+        match = re.search("^M[LT][0-9]+.*$", f050a)
+    if not match:
+        continue
+
+    collections = ["a", f001, "b", "35", "c", u"sid-35-col-hathi"]
+    marcrecord.add("980", subfields=collections)
+
+    outputfile.write(marcrecord.as_marc())
+   
+outputfile.close()
