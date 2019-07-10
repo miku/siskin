5632d0b11638a13d9aedddcb42b25e473c7da4c8
Martin Czygan <martin.czygan@gmail.com>
2019-07-10 18:30:05 +0200

    49: multiple prefixes possible, refs #13587

--- siskin/sources/crossref.py
+++ siskin/sources/crossref.py
@@ -623,6 +623,7 @@ class CrossrefPrefixList(CrossrefTask):
         return CrossrefMembers(date=self.date)
 
     def run(self):
+        """ XXX: multiple prefixes per name """
         output = shellout("jq -rc '.message.items[0].prefix[] | [.value, .name] | @tsv' < {input} > {output}",
                           input=self.input().path)
         luigi.LocalTarget(output).move(self.output().path)
