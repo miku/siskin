f1dc63db96db22fb4a61b28efa3c53541c45f7de
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-09-03 09:22:39 +0200

    156, refs #11786 * add docs * use global format mapping * use global isbn and issn check

--- siskin/assets/156/156_marcbinary.py
+++ siskin/assets/156/156_marcbinary.py
@@ -1,13 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding: utf-8
+#
+# Copyright 2019 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Robert Schenk, <robert.schenk@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
-from __future__ import print_function
+"""
+
+Source: Umweltbibliothek Leipzig
+SID: 156
+Ticket: #11786
+
+"""
 
 import re
 import sys
 
 import marcx
 import pymarc
+from siskin.mappings import formats
+from siskin.utils import check_isbn, check_issn
 
 copytags = ("100", "105", "120", "130", "150", "174", "200", "245", "246", "250", "260", "300", "335", "336", "337",
             "338", "351", "361", "400", "500", "520", "650", "689", "700", "710", "800")
@@ -30,50 +59,38 @@ for i, oldrecord in enumerate(oldrecords, start=1):
         continue
 
     newrecord = marcx.Record(force_utf8=True)
+    newrecord.strict = False
+
+    # pauschale Festlegung
+    format = "Book"
 
     # leader
     leader = "     " + oldrecord.leader[5:]
     newrecord.leader = leader
 
-    # 001
+    # Identifikator
     f001 = str(i)
     newrecord.add("001", data="finc-156-" + f001)
 
+    # Zugangsart
+    f007 = formats[format]["p007"]
+    newrecord.add("007", data=f007)
+
     # ISBN
     try:
-        f020a = oldrecord["020"]["a"]
+        isbn = oldrecord["020"]["a"]
     except:
-        f020a = ""
-
-    if f020a != "":
-        f020a = f020a.replace(" ", "-")
-        f020a = f020a.replace(".", "-")
-        regexp = re.search("([0-9xX-]{10,17})", f020a)
-
-        if regexp:
-            f020a = regexp.group(1)
-            f020a = f020a.rstrip("-")
-            newrecord.add("020", a=f020a)
-        else:
-            print(u"Die ISBN %s konnte nicht mittels regulärer Ausdrücke überprüft werden." % f020a, file=sys.stderr)
+        isbn = ""
+    f020a = check_isbn(isbn)
+    newrecord.add("020", a=f020a)
 
     # ISSN
     try:
-        f022a = oldrecord["022"]["a"]
+        issn = oldrecord["022"]["a"]
     except:
-        f022a = ""
-
-    if f022a != "":
-        f022a = f022a.replace(" ", "-")
-        f022a = f022a.replace(".", "-")
-        regexp = re.search("([0-9xX-]{9})", f022a)
-
-        if regexp:
-            f022a = regexp.group(1)
-            f022a = f022a.rstrip("-")
-            newrecord.add("022", a=f022a)
-        else:
-            print(u"Die ISSN %s konnte nicht mittels regulärer Ausdrücke überprüft werden." % f022a, file=sys.stderr)
+        issn = ""
+    f022a = check_issn(issn)
+    newrecord.add("022", a=f022a)
 
     # Originalfelder, die ohne Änderung übernommen werden
     for tag in copytags:
