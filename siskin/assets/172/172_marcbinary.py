13b6ce104f5c945ef6e44c202c135aaf57f9ecb4
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-10-10 17:19:46 +0200

    172: update mapping script, refs #14442

--- siskin/assets/172/172_marcbinary.py
+++ siskin/assets/172/172_marcbinary.py
@@ -54,70 +54,117 @@ records = inputfile.readlines()
 
 for line in records[1:]:
 
-    fields = line.split("   ")
+    fields = line.split("\t")
 
     marcrecord = marcx.Record(force_utf8=True)
     marcrecord.strict = False
 
+    # Formatfestlegung
+    format = "Article"
+
     # Leader
-    marcrecord.leader = "     naa  22        4500"
+    leader = formats[format]["Leader"]
+    marcrecord.leader = leader
 
     # ID
     f001 = fields[0]
+    f001 = f001.split("-")
+    f001 = f001[2]
     marcrecord.add("001", data="finc-172-" + f001)
 
-    # 007
-    marcrecord.add("007", data="cr")
+    # Zugangstyp
+    f007 = formats[format]["e007"]
+    marcrecord.add("007", data=f007)
+
+    # 008
+    language = fields[7]
+    if language == "engrish":
+        language = "eng"
+    elif language == "English":
+        language = "eng"
+    elif language == "Deutsch":
+        language = "ger"
+    year = fields[12]
+    year = year.replace("\n", "")
+    periodicity = formats[format]["008"]
+    f008 = marc_build_field_008(year, periodicity, language)
+    marcrecord.add("008", data=f008)
 
     # Sprache
-    f041a = fields[10]
-    marcrecord.add("041", a=f041a)
+    if len(language) == 3:
+        marcrecord.add("041", a=language)
 
     # 1. Schöpfer
-    persons = fields[4].split("; ")
-    f100a = persons[0]
-    marcrecord.add("100", a=f100a)
+    persons = fields[4]
+    if persons:
+        num_persons = persons.split(" ")
+        if len(num_persons) > 3:
+            more_than_one_persons = True
+        else:
+            more_than_one_persons = False
+        if more_than_one_persons and "; " in persons:
+            persons = persons.split("; ")
+        elif more_than_one_persons and ", " in persons:
+            persons = persons.split(", ")
+        else:
+            persons = persons.split("*")
+        f100a = persons[0]
+        marcrecord.add("100", a=f100a)
 
     # Haupttitel
-    f245a = fields[6]
-    marcrecord.add("245", a=f245a)
+    f245a = fields[1]
+    f245b = fields[6]
+    if not f245a:
+        f245a = fields[6]
+        f245b = ""
+    if f245a == f245b:
+        f245b = ""
+    f245c = fields[4]
+    marcrecord.add("245", a=f245a, b=f245b, c=f245c)
 
-    # Erscheinungsjahr
-    f260c = fields[13]
+    # Erscheinungsvermerk
+    f260b = fields[8]
+    f260c = fields[12]
     f260c = f260c.replace("\n", "")
     if len(f260c) == 4:
-        marcrecord.add("260", c=f260c)
+        marcrecord.add("260", b=f260b, c=f260c)
 
-    # Reihe
-    f490a = fields[2]
-    marcrecord.add("490", a=f490a)
+    # RDA-Inhaltstyp
+    f336b = formats[format]["336b"]
+    marcrecord.add("336", b=f336b)
+
+    # RDA-Datenträgertyp
+    f338b = formats[format]["338b"]
+    marcrecord.add("338", b=f338b)
 
     # Rechtehinweis
     f500a = fields[3]
-    marcrecord.add("500", a=f500a)
-
-    # Dateiformat
-    f500a = fields[9]
-    marcrecord.add("500", a="Dateiformat: " + f500a)
+    marcrecord.add("500", a=f500a) 
 
     # Beschreibung
     f520a = fields[1]
     marcrecord.add("520", a=f520a)
 
     # Schlagwörter
-    f650a = fields[11]
+    f650a = fields[9]
     marcrecord.add("650", a=f650a)
+
+    # GND-Inhalts- und Datenträgertyp
+    f655a = formats[format]["655a"]
+    f6552 = formats[format]["6552"]
+    marcrecord.add("655", a=f655a, _2=f6552)
 
     # weitere Schöpfer
     for person in persons[1:]:
         marcrecord.add("700", a=person)
 
     # Link zur Ressource
-    f856u = fields[12]
+    f856u = fields[10]
     marcrecord.add("856", q="text/html", _3="Link zur Ressource", u=f856u)
 
-    # Medienform
-    marcrecord.add("935", b="cofz")
+    # SWB-Inhaltstyp
+    f935c = formats[format]["935c"]
+    marcrecord.add("935", c=f935c)    
 
     # Kollektion
     marcrecord.add("980", a=f001, b="172", c="sid-172-col-opal")
