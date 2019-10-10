3789f04ec1e3e34141f69d4ce21f35f42fc08437
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-10-10 11:03:33 +0200

    172: update docs, refs #14442

--- siskin/assets/172/172_marcbinary.py
+++ siskin/assets/172/172_marcbinary.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding: utf-8
+#
+# Copyright 2019 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+#                   Robert Schenk, <robert.schenk@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
-# SID 172
-# https://projekte.ub.uni-leipzig.de/issues/14442
+"""
+
+Source: 14442
+SID: 172
+Ticket: #14442
+
+"""
+
 
 import sys
 
