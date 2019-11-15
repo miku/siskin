e1e0eb6aa99f0f58f5dec43ff7725acd6e7a650a
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-11-15 17:19:15 +0100

    solrcheckup: fix message text, refs #15656

--- bin/solrcheckup.py
+++ bin/solrcheckup.py
@@ -496,7 +496,7 @@ update_history_and_sourcebyinstitution(conn, sqlite, k10plus, ai)
 
 # 4. Step: Send report
 if len(messages) == 0:
-    messages.append(u"Es wurde keine Unregelmaessigkeiten festgestellt.")
+    messages.append(u"Es wurden keine Unregelmaessigkeiten festgestellt.")
 message = u"\n".join(messages)
 send_message(message)
 
