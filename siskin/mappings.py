53d131429de8091f4c8df6c3b112ee1d465ffe1e
Robert Schenk <schenk@ub.uni-leipzig.de>
2019-11-21 15:51:32 +0100

    mappings: fix doc

--- siskin/mappings.py
+++ siskin/mappings.py
@@ -19,7 +19,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  cf not, see <http://www.gnu.org/licenses/>.
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #
 # @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 """
