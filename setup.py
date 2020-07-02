e3eed0208f84067b78f37e962ba89ab95943e7cc
Robert Schenk <schenk@ub.uni-leipzig.de>
2020-07-02 12:53:56 +0200

    setup: fix typo, refs #17428

--- setup.py
+++ setup.py
@@ -229,7 +229,7 @@ setup(name='siskin',
           'bin/190-fincmarc',
           'bin/191-fincmarc',
           'bin/192-fincmarc',
-          'bin/193-fincmarc'
+          'bin/193-fincmarc',
           'bin/194-fincmarc'
       ],
       entry_points={
