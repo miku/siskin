a356f4399e992d94f5bb05cc70121d3a216b6259
Robert Schenk <schenk@ub.uni-leipzig.de>
2020-08-27 07:38:53 +0200

    setup: add sid 198, refs #18021

--- setup.py
+++ setup.py
@@ -223,7 +223,8 @@ setup(name='siskin',
           'bin/192-fincmarc',
           'bin/193-fincmarc',
           'bin/194-fincmarc',
-          'bin/196-fincmarc'
+          'bin/196-fincmarc',
+          'bin/198-fincmarc'
       ],
       entry_points={
         'console_scripts': [
