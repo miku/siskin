1cf113b057181d5f28ec61af97662bac0555ad78
Martin Czygan <martin.czygan@gmail.com>
2021-11-13 16:02:30 +0100

    crossref: switch to daily chunks

--- siskin/sources/crossref.py
+++ siskin/sources/crossref.py
@@ -209,7 +209,7 @@ class CrossrefHarvest(luigi.WrapperTask, CrossrefTask):
     """
     begin = luigi.DateParameter(default=datetime.date(2006, 1, 1))
     end = luigi.DateParameter(default=datetime.date.today())
-    update = luigi.Parameter(default='months', description='days, weeks or months')
+    update = luigi.Parameter(default='days', description='days, weeks or months')
 
     def requires(self):
         if self.update not in ('days', 'weeks', 'months'):
