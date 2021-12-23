4ff555a18775bd77991aa1b2e987c757c0f4b59b
Martin Czygan <martin.czygan@gmail.com>
2021-12-23 10:31:52 +0100

    base: keep all documents

--- siskin/sources/base.py
+++ siskin/sources/base.py
@@ -131,7 +131,7 @@ class BaseSingleFile(BaseTask):
             output = shellout(""" {decomp} "{input}" |
                                   {sanitize} |
                                   sed -e 's@"DE-15-FID"@"FID-MEDIEN-DE-15"@' |
-                                  doisniffer |
+                                  doisniffer -S |
                                   pigz -c > {output}""",
                               decomp=decomp,
                               input=realpath,
