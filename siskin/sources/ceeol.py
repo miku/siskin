77937391d00627dadec169a16d805ce81954fdd8
Martin Czygan <martin.czygan@gmail.com>
2023-03-23 13:26:01 +0100

    53: update timestamp

--- siskin/sources/ceeol.py
+++ siskin/sources/ceeol.py
@@ -60,7 +60,7 @@ class CeeolTask(DefaultTask):
 
     TAG = "53"
 
-    date = luigi.DateParameter(default=datetime.date(2021, 7, 14))
+    date = luigi.DateParameter(default=datetime.date(2023, 3, 20))
 
 
 class CeeolIntermediateSchema(CeeolTask):
