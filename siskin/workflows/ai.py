9930b7c79d4c7d3c31a0cf2287d73bbf5abcad4e
Martin Czygan <martin.czygan@gmail.com>
2023-05-05 13:32:14 +0200

    ai: drop 34 from deduplication

--- siskin/workflows/ai.py
+++ siskin/workflows/ai.py
@@ -269,7 +269,7 @@ class AIInstitutionChanges(AITask):
     def run(self):
         output = shellout(
             """
-            groupcover -lower -prefs '85 55 89 60 50 105 34 101 53 49 28 48 121' < {input} > {output}
+            groupcover -lower -prefs '85 55 89 60 50 105 101 53 49 28 48 121' < {input} > {output}
             """,
             input=self.input().path,
         )
